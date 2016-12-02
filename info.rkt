2955736e0500c580bdf723ad7bdf0e6d86b2d5df
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-02 22:32:20 +0800

    Delete scribblings: I will rewrite it later

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define collection "cheat")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/cheat.scrbl" ())))
+;(define scribblings '(("scribblings/cheat.scrbl" 'main-doc 'omit)))
 (define pkg-desc "Define Opaque Predicates to Cheat Typed Racket")
 (define version "1.0")
 (define pkg-authors '(wargrey))
--- scribblings/cheat.scrbl
+++
@@ -1,9 +0,0 @@
-#lang scribble/manual
-@require[@for-label[typed/cheat racket/base]]
-
-@title{Define Opaque Predicates to Cheat Typed Racket}
-@author{wargrey}
-
-@defmodule[typed/cheat]
-
-@italic{Sorry, but I will write it later as soon as I have time.}
