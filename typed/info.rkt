0a8746ffdd00b9202ce90d41a727d9e441584978
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-03 00:23:37 +0800

    Update Mission: info.rkt: change the doc entry to default

--- typed/info.rkt
+++ typed/info.rkt
@@ -1,5 +1,5 @@
 #lang info
 
 (define collection "cheat")
-(define scribblings '(["tamer/handbook.scrbl" (main-doc) (other)]))
+(define scribblings '(["tamer/handbook.scrbl" (main-doc) (library)]))
 (define pkg-desc "Define Opaque Predicates to Cheat Typed Racket")
