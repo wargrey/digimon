848e049111fb1bb61eed9a82b7f715c4bd526428
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-03 19:09:45 +0800

    Update Mission: return back to single collection package

--- info.rkt
+++ info.rkt
@@ -1,7 +1,12 @@
 #lang info
 
-(define collection 'multi)
+(define collection "digitama")
+(define pkg-desc "The (typed) core to build the digital world")
+
 (define deps '("base" "typed-racket-lib"))
 (define build-deps '("scribble-lib" "racket-doc"))
+
 (define version "1.0")
 (define pkg-authors '(wargrey))
+
+(define scribblings '(["tamer/handbook.scrbl" (main-doc)]))
--- typed/info.rkt
+++
@@ -1,5 +0,0 @@
-#lang info
-
-(define collection "digitama")
-(define scribblings '(["tamer/handbook.scrbl" (main-doc)]))
-(define pkg-desc "The (typed) core to build the digital world")
