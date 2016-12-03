a46fedaad2f2917f8bb9dd40a9101467907ac0ce
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-04 03:59:12 +0800

    Bugfix digitama: add digitama/main.rkt

--- digitama/world.rkt
+++ digitama/world.rkt
@@ -3,5 +3,7 @@
 (provide (all-from-out typed/racket/no-check)
          (all-from-out "cheat.rkt" "main.rkt"))
 
-(require "cheat.rkt")
-(require "main.rkt")
+(require (only-in "cheat.rkt" define-cheat-opaque define/make-is-a?))
+
+(require "main.rkt"
+         (only-in "../cheat.rkt" make-cheat-opaque?))
