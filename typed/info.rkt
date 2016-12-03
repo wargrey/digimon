a19c66154145b9cf5d443776c64670084cd5b77b
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-03 18:38:29 +0800

    Rename Mission: digicore -> digitama

--- typed/info.rkt
+++ typed/info.rkt
@@ -1,5 +1,5 @@
 #lang info
 
-(define collection "digicore")
+(define collection "digitama")
 (define scribblings '(["tamer/handbook.scrbl" (main-doc)]))
 (define pkg-desc "The (typed) core to build the digital world")
