d759f85d4ee6169bce0ee9c023735060e469ef75
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-05 23:39:14 +0800

    Update Mission: define languages digimon/no-check digimon/gui/no-check

--- gui/no-check.rkt
+++ gui/no-check.rkt
@@ -6,3 +6,12 @@
 (require racket/require typed/private/no-check-helper
          (subtract-in typed/racket/gui typed/private/no-check-helper)
          (subtract-in (except-in "../no-check.rkt" exn:fail:object) typed/private/no-check-helper))
+
+(module reader syntax/module-reader
+  #:read digimon-read
+  #:read-syntax digimon-read-syntax
+  #:language 'digimon/gui/no-check
+  #:language-info '#(digimon/language-info digimon-get-info ())
+  #:info digimon-info
+
+  (require digimon/language-info))
