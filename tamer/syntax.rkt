4459850845236b9e7902d8fdb35bca343f4f4e4f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-06 20:16:26 +0800

    Update Mission: require some useful module for syntax

---
+++ tamer/syntax.rkt
@@ -0,0 +1,43 @@
+#lang racket/base
+
+(module t1 digimon
+  (define-syntax (t stx)
+    (syntax-parse stx
+      [(_ who-cares ...)
+       (for/list ([s (in-syntax #'(who-cares ...))]) (syntax-e s))
+       #'(void)])))
+  
+(module t2 digimon/no-check
+  (define-syntax (t stx)
+    (syntax-parse stx
+      [(_ who-cares ...)
+       (for/list ([s (in-syntax #'(who-cares ...))]) (syntax-e s))
+       #'(void)])))
+
+(module t3 digimon/gui
+  (define-syntax (t stx)
+    (syntax-parse stx
+      [(_ who-cares ...)
+       (for/list ([s (in-syntax #'(who-cares ...))]) (syntax-e s))
+       #'(void)])))
+
+(module t4 digimon/gui/no-check
+  (define-syntax (t stx)
+    (syntax-parse stx
+      [(_ who-cares ...)
+       (for/list ([s (in-syntax #'(who-cares ...))]) (syntax-e s))
+       #'(void)])))
+
+(module t5 digimon/sugar
+  (define-syntax (t stx)
+    (syntax-parse stx
+      [(_ who-cares ...)
+       (for/list ([s (in-syntax #'(who-cares ...))]) (syntax-e s))
+       #'(void)])))
+
+(module t6 digimon/ffi
+  (define-syntax (t stx)
+    (syntax-parse stx
+      [(_ who-cares ...)
+       (for/list ([s (in-syntax #'(who-cares ...))]) (syntax-e s))
+       #'(void)])))
