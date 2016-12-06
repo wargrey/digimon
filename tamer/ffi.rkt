043861896e21e5dc2df710f5e6ef3fe2704cc690
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2016-12-06 20:22:40 +0800

    Bugfix tamer: ffi.rkt: digimon/ffi does not provide typed APIs

--- tamer/ffi.rkt
+++ tamer/ffi.rkt
@@ -1,5 +1,7 @@
 #lang digimon/ffi
 
+(require "../openssl.rkt")
+
 (define ctx (make-EVP_MD_CTX))
 (for ([hash (in-list (list md5 mdc2 dss dss1 ripemd160 sha1 sha224 sha256 sha384 sha512))]
       [hashname (in-list '(md5 mdc2 dss dss1 ripemd160 sha1 sha224 sha256 sha384 sha512))])
