fc98f613acd8ee3ddd2f935538d8dd56d6777e5f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2017-03-29 16:17:17 +0800

    now clients can custom their own tongue folder

--- tongue.rkt
+++ tongue.rkt
@@ -1,12 +1,10 @@
 #lang typed/racket
 
-(provide speak ~speak digimon-tongue-paths)
+(provide speak ~speak)
 (provide current-tongue all-tongues default-tongue default-fallback-tongue)
+(provide default-tongue-paths default-tongue-extension default-tongue-fold)
 
 (require "system.rkt")
-
-;;; Although Racket has its own i18n solution, it is binded too close with DrRacket.
-;;; So I write my own solution based on its idea, but more elegant.
 
 ;; The first regexp is used in Windows and the second is used on other platforms.
 ;; All regexps are compared to the result of (system-language+country).
@@ -38,42 +36,52 @@
 
 (define current-tongue : (Parameterof Symbol) (make-parameter (default-tongue)))
 (define default-fallback-tongue : (Parameterof Symbol) (make-parameter 'English))
-(define digimon-tongue-paths : (Parameterof (Listof Path-String)) (make-parameter null))
+(define default-tongue-paths : (Parameterof (Listof Path-String)) (make-parameter null))
+(define default-tongue-extension : (Parameterof Bytes) (make-parameter #".rktl"))
+(define default-tongue-fold : (Parameterof (Option (-> Path (HashTable Symbol String) (HashTable Symbol String)))) (make-parameter #false))
 
 (define all-tongues : (-> (Listof Symbol))
   (lambda []
-    (sort (remove-duplicates
-           (for*/fold ([languages : (Listof Symbol) null])
-                      ([digimon-tongue (in-list (digimon-tongue-paths))]
-                       #:when (directory-exists? digimon-tongue)
-                       [tongue.rktl : Path (in-list (directory-list digimon-tongue))]
-                       #:when (equal? (filename-extension tongue.rktl) #"rktl"))
-             (cons (string->symbol (string-trim (path->string tongue.rktl) #px"\\.rktl")) languages)))
-          symbol<?)))
+    (define all : (Listof Symbol)
+      (for*/fold ([languages : (Listof Symbol) null])
+                 ([digimon-tongue (in-list (default-tongue-paths))]
+                  #:when (directory-exists? digimon-tongue)
+                  [tongue.rktl : Path (in-list (directory-list digimon-tongue))]
+                  #:when (equal? (path-get-extension tongue.rktl) (default-tongue-extension)))
+        (cons (string->symbol (path->string (path-replace-extension tongue.rktl #""))) languages)))
+    (sort (remove-duplicates all) symbol<?)))
 
 (define speak : (-> Symbol [#:in Symbol] String)
-  (let ([dicts : (HashTable Symbol (HashTable Symbol String)) (make-hasheq)]
-        [topic : Symbol 'exn:read:tongue])
-    (lambda [word #:in [tongue (current-tongue)]]
-      (unless (hash-has-key? dicts tongue)
-        (hash-set! dicts tongue
-                   (for/fold ([dictionary : (HashTable Symbol String) (make-immutable-hasheq)])
-                             ([digimon-tongue (in-list (digimon-tongue-paths))])
-                     (define tongue.rktl : Path (build-path digimon-tongue (~a tongue #".rktl")))
-                     (define records : Any (with-handlers ([exn? (λ [[e : exn]] e)]) (with-input-from-file tongue.rktl read)))
-                     (cond [(list? records)
-                            (for/fold ([dict : (HashTable Symbol String) dictionary])
-                                      ([record (in-list records)])
-                              (match record
-                                [(cons (? symbol? word) (? string? content)) (hash-set dict word content)]
-                                [else (dtrace-warning #:topic topic "~a: ~s" tongue.rktl record) dict]))]
-                           [(exn? records) (dtrace-warning #:topic topic "~a: ~a" tongue.rktl (exn-message records)) dictionary]
-                           [else (dtrace-warning #:topic topic "~a: ~s" tongue.rktl records) dictionary]))))
-      (hash-ref (hash-ref dicts tongue) word
-                (thunk (cond [(symbol=? tongue (default-fallback-tongue)) (string-replace (symbol->string word) "-" " ")]
-                             [else (speak word #:in (default-fallback-tongue))]))))))
+  (lambda [word #:in [tongue (current-tongue)]]
+    (unless (hash-has-key? dicts tongue)
+      (define fold-tongue : (-> Path (HashTable Symbol String) (HashTable Symbol String)) (or (default-tongue-fold) default-fold-tongue))
+      (hash-set! dicts tongue
+                 (for/fold ([dictionary : (HashTable Symbol String) (make-immutable-hasheq)])
+                           ([digimon-tongue (in-list (default-tongue-paths))])
+                   (define tongue.rktl : Path (build-path digimon-tongue (~a tongue (default-tongue-extension))))
+                   (fold-tongue tongue.rktl dictionary))))
+    (hash-ref (hash-ref dicts tongue) word
+              (thunk (cond [(symbol=? tongue (default-fallback-tongue)) (string-replace (symbol->string word) "-" " ")]
+                           [else (speak word #:in (default-fallback-tongue))])))))
 
 (define ~speak : (-> Symbol [#:in Symbol] Any * String)
   (lambda [word #:in [tongue (current-tongue)] . argl]
     (cond [(null? argl) (speak word #:in tongue)]
           [else (apply format (speak word #:in tongue) argl)])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define topic : Symbol 'exn:read:tongue)
+(define dicts : (HashTable Symbol (HashTable Symbol String)) (make-hasheq))
+
+(define default-fold-tongue : (-> Path (HashTable Symbol String) (HashTable Symbol String))
+  (lambda [tongue.rktl dictionary]
+    (define dictionary : (HashTable Symbol String) (make-immutable-hasheq))
+    (define records : Any (with-handlers ([exn? (λ [[e : exn]] e)]) (with-input-from-file tongue.rktl read)))
+    (cond [(list? records)
+           (for/fold ([dict : (HashTable Symbol String) dictionary])
+                     ([record (in-list records)])
+             (match record
+               [(cons (? symbol? word) (? string? content)) (hash-set dict word content)]
+               [else (dtrace-warning #:topic topic "~a: ~s" tongue.rktl record) dict]))]
+          [(exn? records) (dtrace-warning #:topic topic "~a: ~a" tongue.rktl (exn-message records)) dictionary]
+          [else (dtrace-warning #:topic topic "~a: ~s" tongue.rktl records) dictionary])))
