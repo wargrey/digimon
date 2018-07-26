1b8e9e2e033d0e320b8ffc284a2937b30de1443f
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2018-07-26 08:40:47 +0800

    add source location related macros

--- digitama/sugar.rkt
+++ digitama/sugar.rkt
@@ -3,6 +3,8 @@
 (provide (all-defined-out))
 
 (require racket/match)
+(require racket/path)
+(require racket/list)
 
 (require (for-syntax racket/base))
 (require (for-syntax racket/syntax))
@@ -39,16 +41,29 @@
       (if (not rmp) '<nota-module> (resolved-module-path-name rmp))))
 
 (define-syntax (#%file stx)
-  #'(let ([full (ann (#%full-module) (U Symbol Path))])
+  #'(let ([full (ann (#%full-module) (U Symbol Path (Pairof Path (Listof Symbol))))])
       (cond [(path? full) full]
             [(pair? full) (car full)]
             [else (current-directory)])))
 
 (define-syntax (#%module stx)
-  #'(let ([full (ann (#%full-module) (U Symbol Path))])
+  #'(let ([full (ann (#%full-module) (U Symbol Path (Pairof Path (Listof Symbol))))])
       (cond [(path? full) (string->symbol (path->string (path-replace-extension (assert (file-name-from-path full) path?) "")))]
             [(pair? full) (last (cdr full))]
             [else '<anonymous>])))
+
+(define-syntax (#%line stx)
+  #`(quote #,(syntax-line stx)))
+
+(define-syntax (#%column stx)
+  #`(quote #,(syntax-column stx)))
+
+(define-syntax (#%position stx)
+  #`(quote #,(syntax-position stx)))
+
+(define-syntax (#%location stx)
+  #`(cons #,(syntax-line stx)
+          #,(syntax-column stx)))
 
 (define-syntax (#%function stx) ; class method has a symbol name looks like "[name] method in [class%]"
   #'(let use-next-id : Symbol ([stacks (continuation-mark-set->context (current-continuation-marks))])
