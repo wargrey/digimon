b582e1ba76b6eba2b43f0e19d8faf6e53d535238
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-03-29 10:09:18 +0800

    base64 encoder and decoder accept String

--- binscii.rkt
+++ binscii.rkt
@@ -1,5 +1,15 @@
 #lang typed/racket/base
 
-(provide (all-defined-out) base64-encode base64-decode)
+(provide (all-defined-out))
 
-(require "digitama/bintext/base64.rkt")
+(require (prefix-in base64: "digitama/bintext/base64.rkt"))
+
+(define base64-encode : (-> (U String Bytes) [#:62nd Positive-Byte] [#:63rd Positive-Byte] Bytes)
+  (lambda [/dev/stdin #:62nd [62nd base64:62nd] #:63rd [63rd base64:63rd]]
+    (cond [(bytes? /dev/stdin) (base64:encode /dev/stdin 62nd 63rd)]
+          [else (base64:encode (string->bytes/utf-8 /dev/stdin) 62nd 63rd)])))
+
+(define base64-decode : (-> (U String Bytes) [#:62nd Positive-Byte] [#:63rd Positive-Byte] Bytes)
+  (lambda [/dev/stdin #:62nd [62nd base64:62nd] #:63rd [63rd base64:63rd]]
+    (cond [(bytes? /dev/stdin) (base64:decode /dev/stdin 62nd 63rd)]
+          [else (base64:decode (string->bytes/utf-8 /dev/stdin) 62nd 63rd)])))
--- digitama/bintext/base64.rkt
+++ digitama/bintext/base64.rkt
@@ -5,56 +5,56 @@
 (require racket/list)
 (require racket/unsafe/ops)
 
-(define base64-padding : Positive-Byte #;#\= #x3D)
-(define base64-62nd : Positive-Byte #;#\+ #x2B)
-(define base64-63rd : Positive-Byte #;#\/ #x2F)
+(define padding : Positive-Byte #;#\= #x3D)
+(define 62nd : Positive-Byte #;#\+ #x2B)
+(define 63rd : Positive-Byte #;#\/ #x2F)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define base64-encode : (-> Bytes [#:62nd Positive-Byte] [#:63rd Positive-Byte] Bytes)
-  (lambda [src #:62nd [62nd base64-62nd] #:63rd [63rd base64-63rd]]
+(define encode : (-> Bytes Positive-Byte Positive-Byte Bytes)
+  (lambda [src 62nd 63rd]
     (define ssize : Index (bytes-length src))
     (define-values (q r) (quotient/remainder ssize 3))
-    (define dest : Bytes (make-bytes (unsafe-fx* 4 (+ q (min r 1))) base64-padding))
+    (define dest : Bytes (make-bytes (unsafe-fx* 4 (+ q (min r 1))) padding))
     (define dsize : Index (bytes-length dest))
 
-    (unsafe-vector*-set! base64-b2t-table 62 62nd)
-    (unsafe-vector*-set! base64-b2t-table 63 63rd)
+    (unsafe-vector*-set! b2t-table 62 62nd)
+    (unsafe-vector*-set! b2t-table 63 63rd)
     
     (let translate ([sidx : Nonnegative-Fixnum 0]
                     [didx : Nonnegative-Fixnum 0])
       (when (and (< sidx ssize) (< didx dsize))
-        (base64-binary-to-text src sidx (- ssize sidx) dest didx)
+        (binary-to-text src sidx (- ssize sidx) dest didx)
         (translate (+ sidx 3) (+ didx 4))))
     
     dest))
 
-(define base64-decode : (-> Bytes [#:62nd Positive-Byte] [#:63rd Positive-Byte] Bytes)
-  (lambda [src #:62nd [62nd base64-62nd] #:63rd [63rd base64-63rd]]
+(define decode : (-> Bytes Positive-Byte Positive-Byte Bytes)
+  (lambda [src 62nd 63rd]
     (define spsize : Index (bytes-length src))
     (define ssize : Index
       (cond [(< spsize 3) spsize]
-            [(= (unsafe-bytes-ref src (- spsize 2)) base64-padding) (assert (- spsize 2) index?)]
-            [(= (unsafe-bytes-ref src (- spsize 1)) base64-padding) (assert (- spsize 1) index?)]
+            [(= (unsafe-bytes-ref src (- spsize 2)) padding) (assert (- spsize 2) index?)]
+            [(= (unsafe-bytes-ref src (- spsize 1)) padding) (assert (- spsize 1) index?)]
             [else spsize]))
 
     (define-values (q r) (quotient/remainder ssize 4))
     (define dest : Bytes (make-bytes (unsafe-fx+ (unsafe-fx* 3 q) (case r [(3) 2] [(2) 1] [else 0]))))
     (define dsize : Index (bytes-length dest))
 
-    (unsafe-vector*-set! base64-t2b-table 62nd 62)
-    (unsafe-vector*-set! base64-t2b-table 63rd 63)
+    (unsafe-vector*-set! t2b-table 62nd 62)
+    (unsafe-vector*-set! t2b-table 63rd 63)
     
     (let translate ([sidx : Nonnegative-Fixnum 0]
                     [didx : Nonnegative-Fixnum 0])
       (when (and (< sidx ssize) (< didx dsize))
-        (base64-text-to-binary src sidx (- ssize sidx) dest didx)
+        (text-to-binary src sidx (- ssize sidx) dest didx)
         (translate (+ sidx 4) (+ didx 3))))
     
     dest))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define-values (base64-b2t-table base64-t2b-table)
-  (let ([ch64s (append (range #x41 #x5B) #;A-Z (range #x61 #x7B) #;a-z (range #x30 #x3A) #;0-9 (list base64-62nd base64-63rd))]
+(define-values (b2t-table t2b-table)
+  (let ([ch64s (append (range #x41 #x5B) #;A-Z (range #x61 #x7B) #;a-z (range #x30 #x3A) #;0-9 (list 62nd 63rd))]
         [b2t-table (make-vector 64 0)] [t2b-table (make-vector #xFF 0)])
     (for ([ch (in-list ch64s)]
           [idx (in-naturals)])
@@ -62,45 +62,45 @@
       (vector-set! t2b-table ch idx))
     (values b2t-table t2b-table)))
 
-(define base64-binary-to-text : (-> Bytes Index Fixnum Bytes Index Void)
+(define binary-to-text : (-> Bytes Index Fixnum Bytes Index Void)
   (lambda [src sidx rest dest didx]
     (case rest
       [(1)
        (let ([bits (unsafe-fxlshift (unsafe-bytes-ref src sidx) 4)])
-         (unsafe-bytes-set! dest (+ didx 1) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits 0 6)))
-         (unsafe-bytes-set! dest (+ didx 0) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits 6 12))))]
+         (unsafe-bytes-set! dest (+ didx 1) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits 0 6)))
+         (unsafe-bytes-set! dest (+ didx 0) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits 6 12))))]
       [(2)
        (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-bytes-ref src sidx) 10) (unsafe-fxlshift (unsafe-bytes-ref src (+ sidx 1)) 2))])
-         (unsafe-bytes-set! dest (+ didx 2) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits 0 6)))
-         (unsafe-bytes-set! dest (+ didx 1) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits 6 12)))
-         (unsafe-bytes-set! dest (+ didx 0) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits 12 18))))]
+         (unsafe-bytes-set! dest (+ didx 2) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits 0 6)))
+         (unsafe-bytes-set! dest (+ didx 1) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits 6 12)))
+         (unsafe-bytes-set! dest (+ didx 0) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits 12 18))))]
       [else
        (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-bytes-ref src (+ sidx 0)) 16)
                                 (unsafe-fxlshift (unsafe-bytes-ref src (+ sidx 1)) 8)
                                 (unsafe-bytes-ref src (+ sidx 2)))])
-         (unsafe-bytes-set! dest (+ didx 3) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits  0  6)))
-         (unsafe-bytes-set! dest (+ didx 2) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits  6 12)))
-         (unsafe-bytes-set! dest (+ didx 1) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits 12 18)))
-         (unsafe-bytes-set! dest (+ didx 0) (unsafe-vector*-ref base64-b2t-table (bitwise-bit-field bits 18 24))))])))
+         (unsafe-bytes-set! dest (+ didx 3) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits  0  6)))
+         (unsafe-bytes-set! dest (+ didx 2) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits  6 12)))
+         (unsafe-bytes-set! dest (+ didx 1) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits 12 18)))
+         (unsafe-bytes-set! dest (+ didx 0) (unsafe-vector*-ref b2t-table (bitwise-bit-field bits 18 24))))])))
 
-(define base64-text-to-binary : (-> Bytes Index Integer Bytes Index Void)
+(define text-to-binary : (-> Bytes Index Integer Bytes Index Void)
   (lambda [src sidx rest dest didx]
     (case rest
       [(3)
-       (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 0))) 10)
-                                (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 1)))  4)
-                                (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 2))) -2))])
+       (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 0))) 10)
+                                (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 1)))  4)
+                                (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 2))) -2))])
          (unsafe-bytes-set! dest (+ didx 1) (bitwise-bit-field bits  0  8))
          (unsafe-bytes-set! dest (+ didx 0) (bitwise-bit-field bits  8 16)))]
       [(2)
-       (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 0)))  2)
-                                (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 1))) -4))])
+       (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 0)))  2)
+                                (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 1))) -4))])
          (unsafe-bytes-set! dest (+ didx 0) (bitwise-bit-field bits  0  8)))]
       [else
-       (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 0))) 18)
-                                (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 1))) 12)
-                                (unsafe-fxlshift (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 2)))  6)
-                                (unsafe-vector*-ref base64-t2b-table (unsafe-bytes-ref src (+ sidx 3))))])
+       (let ([bits (bitwise-ior (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 0))) 18)
+                                (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 1))) 12)
+                                (unsafe-fxlshift (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 2)))  6)
+                                (unsafe-vector*-ref t2b-table (unsafe-bytes-ref src (+ sidx 3))))])
          (unsafe-bytes-set! dest (+ didx 2) (bitwise-bit-field bits  0  8))
          (unsafe-bytes-set! dest (+ didx 1) (bitwise-bit-field bits  8 16))
          (unsafe-bytes-set! dest (+ didx 0) (bitwise-bit-field bits 16 24)))])))
