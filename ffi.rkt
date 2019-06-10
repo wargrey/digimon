7f794438c42f6bd967718c4aa0be55b58ffd6341
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-06-10 16:43:24 +0800

    reprovide ffi/unsafe

--- ffi.rkt
+++ ffi.rkt
@@ -1,19 +1,24 @@
 #lang racket/base
 
 (provide (all-defined-out))
-
-(require (for-syntax racket/base))
-(require (for-syntax syntax/parse))
+(provide (all-from-out ffi/unsafe ffi/unsafe/define))
 
 (require racket/path)
 
 (require ffi/unsafe)
+(require ffi/unsafe/define)
 
+(require (for-syntax racket/base))
+(require (for-syntax syntax/parse))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define-syntax (digimon-ffi-lib stx)
   (syntax-parse stx #:literals []
     [(_ libname
         (~optional (~seq #:global? ?:expr) #:defaults ([? #'#true]))
         (~optional (~seq #:on-fail on-fail:expr) #:defaults ([on-fail #'#false])))
      #'(ffi-lib #:global? ? #:fail on-fail
-                (build-path (path-only (resolved-module-path-name (variable-reference->resolved-module-path (#%variable-reference))))
-                            "compiled" "native" (system-library-subpath #false) libname))]))
+                (let* ([rkt (resolved-module-path-name (variable-reference->resolved-module-path (#%variable-reference)))]
+                       [compiled (build-path (path-only rkt) "compiled" "native" (system-library-subpath #false) libname)]
+                       [shipped (build-path (path-only rkt) (system-library-subpath #false) libname)])
+                  (if (file-exists? compiled) compiled shipped)))]))
