83acd02105bd9ed21435bd253f64bd86e060c05c
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-06-13 19:14:36 +0800

    cleanup ffi.rkt

--- ffi.rkt
+++ ffi.rkt
@@ -1,12 +1,17 @@
 #lang racket/base
 
 (provide (all-defined-out))
-(provide (all-from-out ffi/unsafe ffi/unsafe/define))
+(provide (all-from-out ffi/unsafe))
+(provide (all-from-out ffi/unsafe/define))
+(provide (all-from-out ffi/unsafe/alloc))
 
 (require racket/path)
 
 (require ffi/unsafe)
 (require ffi/unsafe/define)
+(require ffi/unsafe/alloc)
+
+(require "digitama/ffi.rkt")
 
 (require (for-syntax racket/base))
 (require (for-syntax syntax/parse))
@@ -20,7 +25,9 @@
      #'(let ([modpath (variable-reference->module-source (#%variable-reference))]
              [libpath (system-library-subpath #false)])
          (if (not (path? modpath)) ; when distributed as a standalone executable
-             (ffi-lib #:global? ? #:fail on-fail (build-path libpath libname))
+             (ffi-lib (build-path (ffi-distributed-library-path) libpath libname)
+                      #:global? ? #:fail (λ [] (ffi-lib (build-path libpath libname)
+                                                        #:global? ? #:fail on-fail)))
              (let ([this-root (path-only modpath)])
                (ffi-lib (build-path this-root "compiled" "native" libpath libname)
                         #:global? ? #:fail (λ [] (ffi-lib (build-path this-root libpath libname)
