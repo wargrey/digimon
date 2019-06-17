b1f538f28d16cbdc2baaeec309c061529e59afc6
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-06-17 14:30:24 +0800

    add (define-file-loader)

--- filesystem.rkt
+++ filesystem.rkt
@@ -5,6 +5,34 @@
 
 (require racket/path)
 
+(require (for-syntax racket/base))
+(require (for-syntax racket/syntax))
+
+(define-syntax (define-file-loader stx)
+  (syntax-case stx [lambda λ]
+    [(_ id #:+ Type #:lambda do-read)
+     #'(define id : (-> Path-String [#:mode (U 'binary 'text)] Type)
+         (let ([up-to-dates : (HashTable Path (Pairof Nonnegative-Fixnum Type)) (make-hash)]
+               [read-datum : (-> Input-Port Type) do-read])
+           (lambda [src #:mode [mode-flag 'binary]]
+             (define mtime : Nonnegative-Fixnum (file-or-directory-modify-seconds src))
+             (define src-key : Path (simplify-path src))
+             (define mdatum : (Option (Pairof Nonnegative-Fixnum Type)) (hash-ref up-to-dates src-key (λ [] #false)))
+             (cond [(and mdatum (<= mtime (car mdatum))) (cdr mdatum)]
+                   [else (let ([datum (call-with-input-file* src read-datum #:mode mode-flag)])
+                           (hash-set! up-to-dates src-key (cons mtime datum))
+                           datum)]))))]
+    [(_ id #:+ Type (lambda [/dev/stdin] body ...))
+     (with-syntax ([id* (format-id #'id "~a*" (syntax-e #'id))])
+       #'(begin (define id : (-> Input-Port Type)
+                  (lambda [/dev/stdin]
+                    body ...))
+
+                (define-file-loader id* #:+ Type #:lambda id)))]
+    [(_ id #:+ Type (λ [/dev/stdin] body ...))
+     #'(define-file-loader id #:+ Type (lambda [/dev/stdin] body ...))]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define dirname : (-> Path-String [#:rootname String] String)
   (lambda [path #:rootname [root "/"]]
     (define dir : (Option Path-String)
