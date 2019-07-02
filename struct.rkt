767f719a9e20354277d31466285431f520e24693
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-07-02 16:26:56 +0800

    uncover object's interface

--- struct.rkt
+++ struct.rkt
@@ -35,48 +35,53 @@
     [(_ id : ID ([method : MethodType defmth ...] ...))
      #'(define-object id : ID () ([method : MethodType defmth ...] ...))]
     [(_ id : ID ([field : FieldType defval ...] ...) ([method : MethodType defmth ...] ...))
-     (with-syntax* ([api-id (datum->syntax #'id (gensym (format "~a$" (syntax-e #'id))))]
-                    [id-api (format-id #'id "~a-~a" (syntax-e #'id) (syntax-e #'api-id))]
+     (with-syntax* ([ABS-ID (format-id #'id "Abstract-~a" (syntax-e #'ID))]
+                    [abs-id (format-id #'id "abstract-~a" (syntax-e #'id))]
+                    [id-abs (format-id #'id "~a-interface" (syntax-e #'id))]
                     [make-id (format-id #'id "make-~a" (syntax-e #'id))]
+                    [super-id (format-id #'id "super-~a" (syntax-e #'id))]
                     [(kw-args ...)
                      (for/fold ([args null])
                                ([<field> (in-syntax #'(field ... method ...))]
                                 [<Argument> (in-syntax #'([field : FieldType defval ...] ... [method : MethodType defmth ...] ...))])
                        (cons (datum->syntax <field> (string->keyword (symbol->string (syntax-e <field>))))
                              (cons <Argument> args)))]
-                    [([id-field ...] [api-field ...])
-                     (let-values ([(sdleif sdleif-api)
-                                   (for/fold ([sdleif null] [sdleif-api null])
+                    [([id-field ...] [abs-field ...])
+                     (let-values ([(sdleif sdleif-abs)
+                                   (for/fold ([sdleif null] [sdleif-abs null])
                                              ([<field> (in-syntax #'(field ...))])
                                      (define <id-field> (datum->syntax <field> (string->symbol (format "~a-~a" (syntax-e #'id) (syntax-e <field>)))))
-                                     (define <api-field> (datum->syntax <field> (string->symbol (format "~a-~a" (syntax-e #'api-id) (syntax-e <field>)))))
-                                     (values (cons <id-field> sdleif) (cons <api-field> sdleif-api)))])
-                       (list (reverse sdleif) (reverse sdleif-api)))]
-                    [([id-method-apply ...] [id-method ...] [api-method ...])
-                     (let-values ([(sylppa sdleif sdleif-api)
-                                   (for/fold ([sylppa null] [sdleif null] [sdleif-api null])
+                                     (define <abs-field> (datum->syntax <field> (string->symbol (format "~a-~a" (syntax-e #'abs-id) (syntax-e <field>)))))
+                                     (values (cons <id-field> sdleif) (cons <abs-field> sdleif-abs)))])
+                       (list (reverse sdleif) (reverse sdleif-abs)))]
+                    [([id-method-apply ...] [id-method ...] [abs-method ...])
+                     (let-values ([(sylppa sdleif sdleif-abs)
+                                   (for/fold ([sylppa null] [sdleif null] [sdleif-abs null])
                                              ([<method> (in-syntax #'(method ...))])
                                      (define <id-apply> (datum->syntax <method> (string->symbol (format "~a.~a" (syntax-e #'id) (syntax-e <method>)))))
                                      (define <id-field> (datum->syntax <method> (string->symbol (format "~a-~a" (syntax-e #'id) (syntax-e <method>)))))
-                                     (define <api-field> (datum->syntax <method> (string->symbol (format "~a-~a" (syntax-e #'api-id) (syntax-e <method>)))))
-                                     (values (cons <id-apply> sylppa) (cons <id-field> sdleif) (cons <api-field> sdleif-api)))])
-                       (list (reverse sylppa) (reverse sdleif) (reverse sdleif-api)))])
-       #'(begin (struct api-id ([field : FieldType] ... [method : MethodType] ...))
-                (struct id ([api-id : api-id]) #:type-name ID)
+                                     (define <abs-field> (datum->syntax <method> (string->symbol (format "~a-~a" (syntax-e #'abs-id) (syntax-e <method>)))))
+                                     (values (cons <id-apply> sylppa) (cons <id-field> sdleif) (cons <abs-field> sdleif-abs)))])
+                       (list (reverse sylppa) (reverse sdleif) (reverse sdleif-abs)))])
+       #'(begin (struct abs-id ([field : FieldType] ... [method : MethodType] ...) #:transparent #:type-name ABS-ID)
+                (struct id ([interface : abs-id]) #:type-name ID)
 
                 (define (make-id kw-args ...) : ID
-                  (id (api-id field ... method ...)))
+                  (id (abs-id field ... method ...)))
+                
+                (define (super-id kw-args ...) : ABS-ID
+                  (abs-id field ... method ...))
 
                 (define (id-field [self : ID]) : FieldType
-                  (api-field (id-api self)))
+                  (abs-field (id-abs self)))
                 ...
                                              
                 (define (id-method [self : ID]) : MethodType
-                  (api-method (id-api self)))
+                  (abs-method (id-abs self)))
                 ...
                                               
                 (define-syntax (id-method-apply stx)
                   (syntax-case stx []
                     [(_ self argl [... ...])
-                     #'((api-method (id-api self)) argl [... ...])]))
+                     #'((abs-method (id-abs self)) argl [... ...])]))
                 ...))]))
