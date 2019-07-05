077fae5222dcd4cdf561d58d939a6e8276be24b2
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-07-05 17:43:55 +0800

    done (define-exception) right

--- exception.rkt
+++ exception.rkt
@@ -6,34 +6,36 @@
 (require (for-syntax racket/syntax))
 (require (for-syntax syntax/parse))
 
-(define-syntax (define-simple-exception stx)
-  (syntax-case stx [: lambda λ]
-    [(_ exn:ssh:sub parent (fields ...) #:->* (Extra-Type ...) (Optional-Type ...) (lambda [args ... #:+ fmt argl] body ...))
-     (with-syntax* ([make-exn (format-id #'exn:ssh:sub "make-~a" (syntax-e #'exn:ssh:sub))]
-                    [make+exn (format-id #'exn:ssh:sub "make+~a" (syntax-e #'exn:ssh:sub))]
-                    [throw-exn (format-id #'exn:ssh:sub "throw-~a" (syntax-e #'exn:ssh:sub))]
-                    [throw+exn (format-id #'exn:ssh:sub "throw+~a" (syntax-e #'exn:ssh:sub))])
-       #'(begin (struct exn:ssh:sub parent (fields ...))
+(define-syntax (define-exception stx)
+  (syntax-parse stx #:literals [let :]
+    [(_ eid (~optional maybe-parent) ([field : FieldType] ...) (make-message [arg : Type] ...) (~optional (~seq #:log maybe-log-exn:id)))
+     (with-syntax* ([make-exn (format-id #'eid "make-~a" (syntax-e #'eid))]
+                    [make+exn (format-id #'eid "make+~a" (syntax-e #'eid))]
+                    [throw-exn (format-id #'eid "throw-~a" (syntax-e #'eid))]
+                    [throw+exn (format-id #'eid "throw+~a" (syntax-e #'eid))]
+                    [parent (or (attribute maybe-parent) #'exn:fail)]
+                    [log-exn (or (attribute maybe-log-exn) #'default-log-error)])
+       #'(begin (struct eid parent ([field : FieldType] ...) #:transparent)
 
-                (define make-exn : (->* (Extra-Type ... String) (Optional-Type ...) #:rest Any exn:ssh:sub)
-                  (lambda [args ... fmt . argl]
-                    body ...))
+                (define make-exn : (-> Any Type ... FieldType ... String Any * eid)
+                  (lambda [src arg ... field ... fmt . argl]
+                    (eid (make-message src arg ... field ... (default-exn-message fmt argl))
+                         (current-continuation-marks)
+                         field ...)))
 
-                (define make+exn : (->* (Extra-Type ... String) (Optional-Type ...) #:rest Any exn:ssh:sub)
-                  (lambda [args ... fmt . argl]
-                    (let ([errobj (apply make-exn args ... fmt argl)])
-                      (ssh-log-error errobj)
+                (define make+exn : (->* (Any Type ... FieldType ... String) (#:logger Logger #:level Log-Level) #:rest Any eid)
+                  (lambda [src arg ... field ... #:logger [logger (current-logger)] #:level [level 'error] fmt . argl]
+                    (let ([errobj (make-exn src arg ... field ... (default-exn-message fmt argl))])
+                      (log-exn errobj #:logger logger #:level level)
                       errobj)))
 
-                (define throw-exn : (->* (Extra-Type ... String) (Optional-Type ...) #:rest Any Nothing)
-                  (lambda [args ... fmt . argl]
-                    (raise (apply make-exn args ... fmt argl))))
+                (define throw-exn : (-> Any Type ... FieldType ... String Any * Nothing)
+                  (lambda [src arg ... field ... fmt . argl]
+                    (raise (make-exn src arg ... field ... (default-exn-message fmt argl)))))
 
-                (define throw+exn : (->* (Extra-Type ... String) (Optional-Type ...) #:rest Any Nothing)
-                  (lambda [args ... fmt . argl]
-                    (raise (apply make+exn args ... fmt argl))))))]
-    [(_ exn:ssh:sub parent (fields ...) #:->* (Extra-Type ...) (Optional-Type ...) (λ [args ... #:+ fmt argl] body ...))
-     #'(define-ssh-error exn:ssh:sub parent (fields ...) #:->* (Extra-Type ...) (Optional-Type ...) (lambda [args ... #:+ fmt argl] body ...))]))
+                (define throw+exn : (->* (Any Type ... FieldType ... String) (#:logger Logger #:level Log-Level) #:rest Any Nothing)
+                  (lambda [src arg ... field ... #:logger [logger (current-logger)] #:level [level 'error] fmt . argl]
+                    (raise (make+exn src arg ... field ... #:logger logger #:level level (default-exn-message fmt argl)))))))]))
 
 (define-syntax (throw stx)
   (syntax-parse stx
@@ -43,3 +45,14 @@
      #'(throw [st argl ...] (#%function) frmt v ...)]
     [(_ [st:id argl ...] src frmt:str v ...)
      #'(raise (st (format (string-append "~s: " frmt) src v ...) (current-continuation-marks) argl ...))]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define default-exn-message : (-> String (Listof Any) String)
+  (lambda [msgfmt argl]
+    (if (null? argl) msgfmt (apply format msgfmt argl))))
+
+(define default-log-error : (->* (exn) (#:logger Logger #:level Log-Level) Void)
+  (lambda [errobj #:logger [logger (current-logger)] #:level [level 'error]]
+    (log-message logger level #false
+                 (format "~a: ~a" (object-name errobj) (exn-message errobj))
+                 errobj)))
