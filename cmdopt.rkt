bc191fca7e3fd5cbc6de7a1de8f7f599bb75178b
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-08-11 23:46:54 +0800

    finish cmdopt-display-flags

--- cmdopt.rkt
+++ cmdopt.rkt
@@ -11,25 +11,83 @@
 (require (for-syntax "digitama/cmdopt.rkt"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define-syntax (command-line stx)
+(define-syntax (define-cmdopt-parser stx)
   (syntax-parse stx #:datum-literals [:]
-    [(_ (~optional (~seq #:program name) #:defaults ([name #'(find-system-path 'run-file)]))
-        (~optional (~seq #:argv arguments) #:defaults ([arguments #'(current-command-line-arguments)]))
-        (~optional (~seq #:args args-form (~optional (~seq (~or #: :) Type) #:defaults ([Type #'Void])) body-expr ...) #:defaults ([args-form #'()])))
-     (with-syntax* ([(args [<String> ...] [<args> ...] [<idx> ...]) (cmd-parse-args #'args-form)])
-       #'(let ([argv : (Vectorof String) arguments])
-           (define main : (-> <String> ... Type)
-             (lambda args
-               (void)
-               body-expr ...))
+    [(_ id:id
+        (~alt (~optional (~seq #:program name) #:defaults ([name #'(find-system-path 'run-file)]))
+              (~optional (~seq #:args args-form (~optional (~seq (~or #: :) Type)) body-expr ...) #:defaults ([args-form #'()] [Type #'Void]))
 
-           (writeln '(-> <String> ... Type))
-           (writeln (map symbol->string '(<args> ...)))
-           (writeln (list '<idx> ...))))]))
+              (~optional (~seq #:multi (~optional mlabel:str) mflags) #:defaults ([mlabel #'"<options> that can be specified any number of times"] [mflags #'()]))
+              (~optional (~seq #:once-each (~optional elabel:str) eflags) #:defaults ([elabel #'"<options> that can be specified at most once"] [eflags #'()]))
+              (~optional (~seq #:once-any (~optional alabel:str) aflags) #:defaults ([alabel #'"<options> that are mutually exclusive"] [aflags #'()]))
+              
+              ; options that occure [0, n) times
+              (~seq #:banner banner:expr)
+              (~seq #:usage-help desc:expr)
+              (~seq #:ps ps:expr))
+        ...)
+     (with-syntax* ([display-help (format-id #'id "display-~a-help" #'id)]
+                    [([msize (mopt mopts ...) string->mflags mdesc ...] ...) (cmd-parse-flags #'mflags)]
+                    [([esize (eopt eopts ...) string->eflags edesc ...] ...) (cmd-parse-flags #'eflags)]
+                    [([asize (aopt aopts ...) string->aflags adesc ...] ...) (cmd-parse-flags #'aflags)]
+                    [(args [<String> ...] [<args> ...] [<idx> ...]) (cmd-parse-args #'args-form)])
+       #`(begin (define display-help : (->* () (Output-Port #:program Any) Void)
+                  (lambda [[/dev/stdout (current-output-port)] #:program [program name]]
+                    (define mwidth : Natural (max 0 msize ...))
+                    (define ewidth : Natural (max 0 esize ...))
+                    (define awidth : Natural (max 0 asize ...))
+                    (define width : Natural (max mwidth ewidth awidth))
+                    
+                    (cmdopt-display-banner /dev/stdout (list banner ...))
+
+                    (fprintf /dev/stdout "usage: ~a" (cmdopt-program-name program))
+                    (when (> width 0) (fprintf /dev/stdout " [<options>]"))
+                    (for ([argv (in-list '(<args> ...))]) (fprintf /dev/stdout " ~a" argv))
+                    (newline /dev/stdout)
+
+                    (cmdopt-display-usage-help /dev/stdout (list desc ...))
+                    
+                    (when (> mwidth 0)
+                      (fprintf /dev/stdout "~n  ~a~n" mlabel)
+                      (cmdopt-display-flags /dev/stdout 'mopt (list 'mopts ...) string->mflags (list mdesc ...) msize width) ...)
+                      
+                    (when (> ewidth 0)
+                      (fprintf /dev/stdout "~n  ~a~n" elabel)
+                      (cmdopt-display-flags /dev/stdout 'eopt (list 'eopts ...) string->eflags (list edesc ...) esize width) ...)
+                    
+                    (when (> awidth 0)
+                      (fprintf /dev/stdout "~n  ~a~n" alabel)
+                      (cmdopt-display-flags /dev/stdout 'aopt (list 'aopts ...) string->aflags (list adesc ...) asize width) ...)
+                    
+                    (cmdopt-display-postscript /dev/stdout (list ps ...))))
+
+                (define id : (->* () ((U (Listof String) (Vectorof String)) #:/dev/hlpout Output-Port #:program Any) Any)
+                  (lambda [[argv (current-command-line-arguments)] #:/dev/hlpout [/dev/hlpout (current-output-port)] #:program [program name]]
+                    (define main : (-> <String> ... Type)
+                      (lambda args
+                        (void)
+                        body-expr ...))
+                    
+                    (writeln '(-> <String> ... Type))
+                    (writeln (map symbol->string '(<args> ...)))
+                    (writeln (list '<idx> ...))))))]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(command-line
- #:program 'nas
- #:argv (vector "ac")
- #:args (src ... dest) #: Flonum
- 1.0)
+(define-cmdopt-parser id
+  #:program 'test
+  #:ps (version)
+  #:usage-help (current-seconds)
+  #:once-each
+  [[(#\v verbose) "Compile with verbose messages"]
+   [(#\p profile) "Compile with profiling"]]
+  #:multi
+  [[(#\l link-flags) "lf" ; flag takes one argument
+                     "Add a flag <lf> for the linker"]]
+  #:once-any
+  [[(#\o optimize-1) "Compile with optimization level 1"]
+   [optimize-2       "Compile with optimization level 2,"
+                     "which includes all of level 1"]]
+  #:args (src ... dest) #: Flonum 
+  1.0)
+
+(display-id-help #:program '(raco wisemon))
--- digitama/cmdopt.rkt
+++ digitama/cmdopt.rkt
@@ -3,14 +3,19 @@
 (provide (all-defined-out))
 (provide (for-syntax (all-defined-out)))
 
+(require racket/path)
+(require racket/string)
+(require racket/format)
+
 (require (for-syntax racket/base))
 (require (for-syntax racket/syntax))
 (require (for-syntax syntax/parse))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (begin-for-syntax
+  (define hlpfmt "<~a>")
+
   (define (cmd-parse-args <args>)
-    (define hlpfmt "<~a>")
     (define args (syntax-e <args>))
     
     (if (pair? args)
@@ -38,6 +43,85 @@
                             (reverse (list* #'* #'String sepyt))
                             (reverse (list* #'[... ...] (format-id args hlpfmt args) spleh))
                             (reverse scidni))]))
-        (list <args> (list #'String #'*) (list (format-id <args> hlpfmt args)) (list)))))
+        (list <args> (list #'String #'*) (list (format-id <args> hlpfmt args)) (list))))
+  
+  (define (cmd-parse-flags <flags>)
+    (syntax-parse <flags>
+      [(deflag ...) (map cmd-parse-flag (syntax->list #'(deflag ...)))]
+      [_ (raise-syntax-error 'cmdopt-parse-flags "malformed flag definition" <flags>)]))
+
+  (define (cmd-parse-flag <flag>)
+    (syntax-parse <flag>
+      [(flag+alias description ...)
+       (define-values (chars words size) (cmd-collect-flag-aliases #'flag+alias))
+       (list* size (append chars words) #false (syntax->datum #'(description ...)))]
+      [_ (raise-syntax-error 'cmdopt-parse-flags "malformed flag definition" <flag>)]))
+
+  (define (cmd-collect-flag-aliases <flag+alias>)
+    (define flags+alias (syntax-e <flag+alias>))
+    (let collect ([srahc null]
+                  [sdrow null]
+                  [flags (if (list? flags+alias) flags+alias (list <flag+alias>))]
+                  [size -2])
+      (if (null? flags)
+          (values (reverse srahc) (reverse sdrow) size)
+          (let* ([<f> (car flags)]
+                 [f (syntax-e <f>)])
+            (cond [(char? f) (collect (cons f srahc) sdrow (cdr flags) (+ size 2 1 1))]
+                  [(symbol? f) (collect srahc (cons f sdrow) (cdr flags) (+ size 2 2 (string-length (symbol->string f))))]
+                  [else (raise-syntax-error 'cmdopt-parse-flags "expected char? or symbol?" <f>)]))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define cmdopt-program-name : (-> Any Any)
+  (lambda [program]
+    (cond [(path? program) (file-name-from-path program)]
+          [(list? program) (string-join (map ~a program))]
+          [else program])))
+
+(define cmdopt-display-banner : (-> Output-Port (Listof Any) Void)
+  (lambda [/dev/stdout banners0]
+    (define banners : (Listof Any) (filter values banners0))
+    
+    (when (pair? banners)
+      (for ([heading (in-list banners)])
+        (displayln heading /dev/stdout))
+
+      (newline /dev/stdout))))
+
+(define cmdopt-display-usage-help : (-> Output-Port (Listof Any) Void)
+  (lambda [/dev/stdout descriptions0]
+    (define descriptions : (Listof Any) (filter values descriptions0))
+    
+    (when (pair? descriptions)
+      (for ([description (in-list descriptions)])
+        (fprintf /dev/stdout "  ~a~n" description)))))
+
+(define cmdopt-display-flags : (-> Output-Port (U Char Symbol) (Listof (U Char Symbol)) (Option Procedure) (Listof Any) Natural Natural Void)
+  (lambda [/dev/stdout flag alias transform descriptions0 this-width max-width]
+    (fprintf /dev/stdout "    ~a~a" (if (char? flag) #\- '--) flag)
+    (for ([alias (in-list alias)])
+      (fprintf /dev/stdout ", ~a~a" (if (char? alias) #\- '--) alias))
+
+    (let ([padding (- max-width this-width)])
+      (when (> padding 0)
+        (fprintf /dev/stdout (make-string padding #\space))))
+
+    (let ([descriptions (filter values descriptions0)])
+      (when (pair? descriptions)
+        (fprintf /dev/stdout "  ~a" (car descriptions))
+        (when (pair? (cdr descriptions))
+          (define leading (make-string (+ max-width 4 2) #\space))
+
+          (for ([desc (in-list (cdr descriptions))])
+            (fprintf /dev/stdout "~n~a~a" leading desc)))
+
+        (newline /dev/stdout)))))
+
+(define cmdopt-display-postscript : (-> Output-Port (Listof Any) Void)
+  (lambda [/dev/stdout ps0]
+    (define postscripts : (Listof Any) (filter values ps0))
+    
+    (when (pair? postscripts)
+      (newline /dev/stdout)
+      (for ([postscript (in-list postscripts)])
+        (fprintf /dev/stdout "~a~n" postscript)))))
