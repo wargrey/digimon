0ad1dfd9e0782d86d4c16433bfbc1c451bd91f68
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2019-08-16 22:38:29 +0800

    finish parse simple flags

--- cmdopt.rkt
+++ cmdopt.rkt
@@ -15,7 +15,7 @@
   (syntax-parse stx #:datum-literals [:]
     [(_ opt:id (~or #: :) Opt
         (~alt (~optional (~seq #:program name) #:defaults ([name #'(find-system-path 'run-file)]))
-              (~optional (~seq #:args args-form (~optional (~seq (~or #: :) Type)) body-expr ...) #:defaults ([args-form #'()] [Type #'Void]))
+              (~optional (~seq #:args args-form) #:defaults ([args-form #'()]))
               
               (~optional (~seq #:multi (~optional mlabel:str) mflags) #:defaults ([mlabel #'"multi-options"] [mflags #'()]))
               (~optional (~seq #:once-each (~optional elabel:str) eflags) #:defaults ([elabel #'"individual options"] [eflags #'()]))
@@ -27,19 +27,36 @@
               (~seq #:ps ps:expr)) ...)
      (with-syntax* ([parse-option (format-id #'opt "parse-~a" #'opt)]
                     [display-option (format-id #'opt "display-~a" #'opt)]
-                    [(flags [mfield MType msize (mopt mopts ...) string->mflags mdesc ...] ...) (cmd-parse-flags #'mflags (make-hasheq))]
-                    [(flags [efield EType esize (eopt eopts ...) string->eflags edesc ...] ...) (cmd-parse-flags #'eflags (syntax-e #'flags))]
-                    [(flags [afield AType asize (aopt aopts ...) string->aflags adesc ...] ...) (cmd-parse-flags #'aflags (syntax-e #'flags))]
-                    [(args [<String> ...] [<args> ...] [<idx> ...]) (cmd-parse-args #'args-form)])
+                    [opt-construct (datum->syntax #'opt (gensym (syntax-e #'opt)))]
+                    [(flags [mfield MType margc msize (moptions ...) string->mflags mopt-ref mdesc ...] ...) (cmd-parse-flags #'mflags (make-hasheq) #true)]
+                    [(flags [efield EType eargc esize (eoptions ...) string->eflags eopt-ref edesc ...] ...) (cmd-parse-flags #'eflags (syntax-e #'flags) #false)]
+                    [(flags [afield AType aargc asize (aoptions ...) string->aflags aopt-ref adesc ...] ...) (cmd-parse-flags #'aflags (syntax-e #'flags) #false)]
+                    [([Type ...] [<args> ...] [idx ...] [ref ...]) (cmd-parse-args #'args-form)])
        #`(begin (struct opt ([mfield : (Listof MType)] ...
                              [efield : (Option EType)] ...
                              [afield : (Option AType)] ...)
+                  #:constructor-name opt-construct
                   #:type-name Opt
                   #:transparent)
-
-                (define parse-option : (->* () ((U (Listof String) (Vectorof String))) (Values Any (Listof String) Boolean))
+                
+                (define parse-option : (->* () ((U (Listof String) (Vectorof String))) (Values Opt Boolean Type ...))
                   (lambda [[argv (current-command-line-arguments)]]
-                    (values (void) null #true)))
+                    (define-values (options multi-options operands help?)
+                      (let ([mfield (λ [opt] : (Pairof Any (List Symbol Byte Symbol)) (cons opt (list 'mfield margc 'multi)))] ...
+                            [efield (λ [opt] : (Pairof Any (List Symbol Byte Symbol)) (cons opt (list 'efield eargc 'once-each)))] ...
+                            [afield (λ [opt] : (Pairof Any (List Symbol Byte Symbol)) (cons opt (list 'afield aargc 'once-any)))] ...)
+                        (cmdopt-parse-arguments 'parse-option argv
+                                                (make-immutable-hasheq (append (map mfield (list 'moptions ...)) ...
+                                                                               (map efield (list 'eoptions ...)) ...
+                                                                               (map afield (list 'aoptions ...)) ...))
+                                                (list 'afield ...))))
+
+                    (define cmdopt : Opt
+                      (opt-construct ((inst mopt-ref MType) multi-options 'mfield string->mflags) ...
+                                     ((inst eopt-ref EType) options 'efield string->eflags) ...
+                                     ((inst aopt-ref AType) options 'afield string->aflags) ...))
+                    
+                    (values cmdopt help? (ref 'parse-option operands idx) ...)))
                 
                 (define display-option : (->* () (Output-Port #:program Any) Void)
                   (lambda [[/dev/stdout (current-output-port)] #:program [program name]]
@@ -59,38 +76,37 @@
                     
                     (when (> mwidth 0)
                       (fprintf /dev/stdout "~n  ~a~n" (cmdopt-help-identity mlabel))
-                      (cmdopt-display-flags /dev/stdout 'mopt (list 'mopts ...) string->mflags (list (cmdopt-help-identity mdesc) ...) msize width) ...)
+                      (cmdopt-display-flags /dev/stdout (list 'moptions ...) (list (cmdopt-help-identity mdesc) ...) msize width) ...)
                       
                     (when (> ewidth 0)
                       (fprintf /dev/stdout "~n  ~a~n" (cmdopt-help-identity elabel))
-                      (cmdopt-display-flags /dev/stdout 'eopt (list 'eopts ...) string->eflags (list (cmdopt-help-identity edesc) ...) esize width) ...)
+                      (cmdopt-display-flags /dev/stdout (list 'eoptions ...) (list (cmdopt-help-identity edesc) ...) esize width) ...)
                     
                     (when (> awidth 0)
                       (fprintf /dev/stdout "~n  ~a~n" (cmdopt-help-identity alabel))
-                      (cmdopt-display-flags /dev/stdout 'aopt (list 'aopts ...) string->aflags (list (cmdopt-help-identity adesc) ...) asize width) ...)
+                      (cmdopt-display-flags /dev/stdout (list 'aoptions ...) (list (cmdopt-help-identity adesc) ...) asize width) ...)
                     
                     (cmdopt-display-postscript /dev/stdout (list ps ...))))))]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define-cmdlet-option cc-flags #: CC-Flags
   #:program 'test
+  #:args (src ... dest)
+
   #:ps (version)
   #:usage-help (current-seconds)
   #:once-each
-  [[(#\v verbose) "Compile with verbose messages"]
-   [(#\p profile) "Compile with profiling"]]
+  [[(#\p profile) "Compile with profiling"]]
   #:multi
-  [[(#\l link-flags) lf ; flag takes one argument
-                     "1-arity option which name is ~~1~1~"
+  [[(#\v verbose) "Compile with verbose messages"]
+   [(#\l link-flags) lf ; flag takes one argument
                      ["Add a flag ~1 for the linker [default: ~a]" null]]]
   #:once-any
   [[(#\o optimize-1) "Compile with optimization level 1"]
    [optimize-2       "Compile with optimization level 2,"
-                     "which includes all of level 1"]]
-  #:args (src ... dest) #: Flonum 
-  1.0)
+                     "which includes all of level 1"]])
 
-(define-values (options names help?) (parse-cc-flags))
+(define-values (options help? srcs dest) (parse-cc-flags))
 
-(when (and help?)
-  (display-cc-flags #:program '(raco wisemon)))
+(cond [(and help?) (display-cc-flags #:program '(raco wisemon))]
+      [else (writeln (list options srcs dest))])
--- digitama/cmdopt.rkt
+++ digitama/cmdopt.rkt
@@ -5,71 +5,57 @@
 
 (require racket/path)
 (require racket/string)
-(require racket/format)
+(require racket/vector)
+(require racket/list)
+
+(require "../format.rkt")
 
 (require (for-syntax racket/base))
 (require (for-syntax racket/syntax))
 (require (for-syntax syntax/parse))
+(require (for-syntax racket/string))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (begin-for-syntax
   (define hlpfmt "<~a>")
+  (define fmargc 255)
 
   (define-syntax-class help
     (pattern (fmt:string vs ...) #:attr value #'(list fmt vs ...))
     (pattern desc:string #:attr value #'desc)
     (pattern otherwise #:attr value (raise-syntax-error 'cmdopt-parse-flags "expect string? or (list*of string? (listof any))" #'otherwise)))
-
-  (define (cmd-parse-args <args>)
-    (define args (syntax-e <args>))
-    
-    (if (pair? args)
-        (let transform ([sgra null]
-                        [sepyt null]
-                        [spleh null]
-                        [scidni null]
-                        [args args])
-          (cond [(pair? args)
-                 (let ([<arg> (car args)])
-                   (cond [(not (eq? (syntax-e <arg>) '...))
-                          (transform (cons <arg> sgra)
-                                     (cons #'String sepyt)
-                                     (cons (format-id <arg> hlpfmt <arg>) spleh)
-                                     (cons (datum->syntax <arg> (length scidni)) scidni)
-                                     (cdr args))]
-                         [(null? (cddr args))
-                          (list (datum->syntax <args> (reverse (cons (cadr args) sgra)))
-                                (reverse (list* #'String #'(Listof String) (cdr sepyt)))
-                                (reverse (list* (format-id (cadr args) hlpfmt (cadr args)) <arg> spleh))
-                                (reverse scidni))]
-                         [else (raise-syntax-error 'cmd-parse-args "misplaced '...'" <arg>)]))]
-                [(null? args) (list <args> (reverse sepyt) (reverse spleh) (reverse scidni))]
-                [else (list <args>
-                            (reverse (list* #'* #'String sepyt))
-                            (reverse (list* #'[... ...] (format-id args hlpfmt args) spleh))
-                            (reverse scidni))]))
-        (list <args> (list #'String #'*) (list (format-id <args> hlpfmt args)) (list))))
   
-  (define (cmd-parse-flags <flags> flagnames)
+  (define (cmd-parse-flags <flags> flagnames multiple?)
     (syntax-parse <flags>
       [(deflag ...) (cons flagnames
                           (for/list ([<deflag> (syntax->list #'(deflag ...))])
-                            (cmd-parse-flag <deflag> flagnames)))]
+                            (cmd-parse-flag <deflag> flagnames multiple?)))]
       [_ (raise-syntax-error 'cmdopt-parse-flags "malformed flag definition" <flags>)]))
 
-  (define (cmd-parse-flag <deflag> flagnames)
-    (syntax-parse <deflag> #:datum-literals [: λ]
-      [(flag+alias (~optional (~seq (~or #:λ λ) string->datum) #:defaults ([string->datum #'values]))
+  (define (cmd-parse-flag <deflag> flagnames multiple?)
+    (syntax-parse <deflag> #:datum-literals [: =>]
+      [(flag+alias (~optional (~seq (~or #:=> =>) string->datum) #:defaults ([string->datum #'values]))
                    arg:id args:id ...
                    (~optional (~seq (~or #: :) Type) #:defaults ([Type #'String]))
                    description:help ...)
        (define-values (flags size name) (cmd-collect-flag-aliases #'flag+alias flagnames))
-       (list* name #'Type size flags #'string->datum
-              (for/list ([<desc> (syntax-e #'(description.value ...))])
-                (cmd-format-description <desc> (syntax->datum #'(arg args ...)) #true)))]
+       (define argv (syntax-e #'(arg args ...)))
+       (define arity (length argv))
+
+       (unless (<= arity fmargc)
+         (let ([over-argv (list-tail argv fmargc)])
+           (raise-syntax-error 'cmdopt-parse-flags (format "no more than ~a arguments" fmargc)
+                               (car over-argv) #false (cdr over-argv))))
+
+       (list* name #'Type arity size flags #'string->datum (if multiple? #'cmdmopt1-ref #'cmdopt1-ref)
+              (let ([argv-data (map syntax-e argv)])
+                (cons (cmd-format-description (cmd-flag-arguments-description arity) arity argv-data #true)
+                      (for/list ([<desc> (syntax-e #'(description.value ...))])
+                        (cmd-format-description <desc> arity argv-data #true)))))]
       [(flag+alias description:help ...)
        (define-values (flags size name) (cmd-collect-flag-aliases #'flag+alias flagnames))
-       (list* name #'Boolean size flags #false (syntax-e #'(description.value ...)))]
+       (list* name #'Boolean 0 size flags #false (if multiple? #'cmdmopt0-ref #'cmdopt0-ref)
+              (syntax-e #'(description.value ...)))]
       [_ (raise-syntax-error 'cmdopt-parse-flags "malformed flag definition" <deflag>)]))
 
   (define (cmd-collect-flag-aliases <flag+alias> flagnames)
@@ -94,8 +80,11 @@
     (define name (cmd-flag->name flag))
 
     (when (hash-has-key? flagnames name)
-      (raise-syntax-error 'cmdopt-parse-flags "duplicate flag"
+      (raise-syntax-error 'cmdopt-parse-flags "duplicate flags"
                           <flag> #false (list (hash-ref flagnames name))))
+
+    (when (memq name '(help -))
+      (raise-syntax-error 'cmdopt-parse-flags "reserved options" <flag>))
 
     (hash-set! flagnames name <flag>)
 
@@ -106,11 +95,10 @@
     (cond [(char? flag) (string->symbol (string flag))]
           [else flag]))
 
-  (define (cmd-format-description <desc> args ~~?)
+  (define (cmd-format-description <desc> arity args ~~?)
     (define desc (syntax-e <desc>))
-    (cond [(list? desc) (list* #'list (cmd-format-description (cadr desc) args #false) (cddr desc))]
-          [else (let ([arity (length args)]
-                      [/dev/stdin (open-input-string desc)]
+    (cond [(list? desc) (list* #'list (cmd-format-description (cadr desc) arity args #false) (cddr desc))]
+          [else (let ([/dev/stdin (open-input-string desc)]
                       [/dev/stdout (open-output-string)])
                   (let ~n ()
                     (define ch (read-char /dev/stdin))
@@ -126,14 +114,155 @@
                                                       [else (write-char #\< /dev/stdout)
                                                             (write (list-ref args (sub1 n)) /dev/stdout)
                                                             (write-char #\> /dev/stdout)]))])
-                                  (~n))])))])))
+                                  (~n))])))]))
+
+  (define (cmd-flag-arguments-description arity)
+    (datum->syntax #false
+                   (string-join (build-list arity
+                                            (λ [i] (format "~~~a" (+ i 1)))))))
+
+  (define (cmd-parse-args <args>)
+    (define args (syntax-e <args>))
+    
+    (if (pair? args)
+        (let transform ([sepyt null]
+                        [spleh null]
+                        [scidni null]
+                        [sfer null]
+                        [args args])
+          (cond [(pair? args)
+                 (let ([<arg> (car args)])
+                   (cond [(not (eq? (syntax-e <arg>) '...))
+                          (transform (cons #'String sepyt)
+                                     (cons (format-id <arg> hlpfmt <arg>) spleh)
+                                     (cons (datum->syntax <arg> (length scidni)) scidni)
+                                     (cons #'cmdarg-ref sfer)
+                                     (cdr args))]
+                         [(and (null? (cddr args)) (pair? sepyt))
+                          (list (reverse (list* #'String #'(Listof String) (cdr sepyt)))
+                                (reverse (list* (format-id (cadr args) hlpfmt (cadr args)) <arg> spleh))
+                                (reverse (list* #'-1 scidni))
+                                (reverse (list* #'cmdarg-ref #'cmdargs-ref (cdr sfer))))]
+                         [else (raise-syntax-error 'cmd-parse-args "misplaced '...'" <arg>)]))]
+                [(null? args) (list (reverse sepyt) (reverse spleh) (reverse scidni) (reverse sfer))]
+                [else ; improper list of arguments
+                 (list (reverse (list* #'* #'String sepyt))
+                       (reverse (list* #'[... ...] (format-id args hlpfmt args) spleh))
+                       (reverse (cons (datum->syntax <args> (length scidni)) scidni))
+                       (reverse #'cmdargs*-ref sfer))]))
+        (list (list #'String #'*) (list (format-id <args> hlpfmt args) #'[... ...])
+              (list #'0) (list #'cmdargs*-ref)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define-type Cmdopt-Help (U String (Pairof String (Listof Any))))
+(define-type Cmdopt-Metaflag (Immutable-HashTable Any (List Symbol Byte Symbol)))
+(define-type Cmdopt-Flags (HashTable Symbol (Listof String)))
+(define-type Cmdopt-MFlags (HashTable Symbol (Listof (Listof String))))
 
-(define cmdopt-parse-arguments : (-> (HashTable Symbol String))
-  (lambda []
-    (make-hasheq)))
+(define vector-null : (Vectorof String) (vector))
+
+(define cmdopt-parse-arguments : (-> Symbol (U (Listof String) (Vectorof String)) Cmdopt-Metaflag (Listof Symbol)
+                                     (Values Cmdopt-Flags Cmdopt-MFlags (Vectorof String) Boolean))
+  (lambda [pname arguments all-flags oa-names]
+    (define options : Cmdopt-Flags (make-hasheq))
+    (define moptions : Cmdopt-MFlags (make-hasheq))
+    (define argv : (Vectorof String) (if (list? arguments) (list->vector arguments) arguments))
+    (define argc : Index (vector-length argv))
+    
+    (let parse ([i : Nonnegative-Fixnum 0]
+                [help? : Boolean #false])
+      (cond [(>= i argc) (values options moptions vector-null help?)]
+            [else (let ([token (vector-ref argv i)])
+                    (cond [(string=? token "") (parse (+ i 1) help?)]
+                          [(string=? token "--") (values options moptions (vector-drop argv (+ i 1)) help?)]
+                          [(string=? token "--help") (parse (+ i 1) #true)]
+                          [(cmdopt-operand? token) (values options moptions (if (eq? i 0) argv (vector-drop argv i)) help?)]
+                          [(eq? (string-ref token 1) #\-)
+                           (let ([--arg (string->symbol (substring token 2))])
+                             (parse (cmdopt-parse-flags pname --arg argv argc all-flags oa-names options moptions (+ i 1)) help?))]
+                          [else (parse (for/fold ([idx : Positive-Fixnum (+ i 1)])
+                                                 ([-a (in-string (substring token 1))])
+                                         (cmdopt-parse-flags pname -a argv argc all-flags oa-names options moptions idx))
+                                       help?)]))]))))
+
+(define cmdopt-parse-flags : (-> Symbol Any (Vectorof String) Index Cmdopt-Metaflag (Listof Symbol) Cmdopt-Flags Cmdopt-MFlags Positive-Fixnum Positive-Fixnum)
+  (lambda [pname -/-- argv argc all-flags oa-names options moptions idx]
+    (define flag.argc.type : (List Symbol Byte Symbol) (hash-ref all-flags -/-- (λ [] (raise-user-error pname "unrecognized option: ~a" -/--))))
+    (define-values (flag fargc type) (values (car flag.argc.type) (cadr flag.argc.type) (caddr flag.argc.type)))
+    
+    (unless (eq? type 'multi)
+      (when (hash-has-key? options flag)
+        (raise-user-error pname "individual option has already been specified: ~a" -/--))
+    
+      (when (eq? type 'once-any)
+        (for ([once-any-flag (in-list oa-names)])
+          (when (hash-has-key? options once-any-flag)
+            (raise-user-error pname "mutually exclusive option(~a) has already been specified: ~a" once-any-flag -/--)))))
+
+    (define-values (fargv nidx)
+      (cond [(= fargc 0) (values (list ".") idx)]
+            [else (let ([nidx (+ idx fargc)]
+                        [given (- argc idx)])
+                    (cond [(> nidx argc) (raise-user-error pname "option(~a) expects ~a, but given ~a" -/-- (~n_w fargc "argument") (~n_w given "argument"))]
+                          [(> fargc 1) (values (build-list fargc (λ [[i : Index]] (vector-ref argv (+ idx i)))) nidx)]
+                          [else (values (list (vector-ref argv idx)) nidx)]))]))
+
+    (cond [(not (eq? type 'multi)) (hash-set! options flag fargv)]
+          [else (hash-set! moptions flag (cons fargv (hash-ref moptions flag (λ [] null))))])
+    
+    nidx))
+
+(define cmdopt-operand? : (-> String Boolean)
+  (lambda [arg]
+    (or (not (eq? (string-ref arg 0) #\-))
+        (string=? arg "-"))))
+
+(define cmdopt0-ref : (All (a) (-> (HashTable Symbol (Listof String)) Symbol False Boolean))
+  (lambda [opts name string->option]
+    (pair? (hash-ref opts name (λ [] null)))))
+
+
+(define cmdopt1-ref : (All (a) (-> (HashTable Symbol (Listof String)) Symbol (-> String a) (Option a)))
+  (lambda [opts name string->option]
+    (define maybe-flag : (Listof String) (hash-ref opts name (λ [] null)))
+
+    (and (pair? maybe-flag)
+         (string->option (car maybe-flag)))))
+
+(define cmdmopt0-ref : (All (a) (-> (HashTable Symbol (Listof (Listof String))) Symbol False (Listof True)))
+  (lambda [opts name string->option]
+    (define flags : (Listof (Listof String)) (hash-ref opts name (λ [] null)))
+    (make-list (length flags) #true)))
+
+(define cmdmopt1-ref : (All (a) (-> (HashTable Symbol (Listof (Listof String))) Symbol (-> String a) (Listof a)))
+  (lambda [opts name string->option]
+    (define flags : (Listof (Listof String)) (hash-ref opts name (λ [] null)))
+    (filter-map (λ [[flags : (Listof String)]] (and (pair? flags) (string->option (car flags))))
+                flags)))
+
+(define cmdarg-ref : (-> Symbol (Vectorof String) Integer String)
+  (lambda [pname argv idx]
+    (define argc : Index (vector-length argv))
+    
+    (cond [(= idx -1) (vector-ref argv (- argc 1))]
+          [(<= argc idx) (raise-user-error pname "insufficient arguments")]
+          [else (vector-ref argv idx)])))
+
+(define cmdargs-ref : (-> Symbol (Vectorof String) Integer (Listof String))
+  (lambda [pname argv idx]
+    (define argc : Index (vector-length argv))
+    (define optc : Integer (- argc idx 1))
+
+    (cond [(<= optc 0) (raise-user-error pname "insufficient arguments")]
+          [else (cmdopt-subvector argv idx optc)])))
+
+(define cmdargs*-ref : (-> Symbol (Vectorof String) Integer (Listof String))
+  (lambda [pname argv idx]
+    null))
+
+(define cmdopt-subvector : (-> (Vectorof String) Integer Integer (Listof String))
+  (lambda [argv idx argc]
+    (build-list argc (λ [[i : Index]] (vector-ref argv (+ idx i))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define cmdopt-program-name : (-> Any Any)
@@ -160,8 +289,10 @@
       (for ([description (in-list descriptions)])
         (fprintf /dev/stdout "  ~a~n" description)))))
 
-(define cmdopt-display-flags : (-> Output-Port (U Char Symbol) (Listof (U Char Symbol)) (Option Procedure) (Listof String) Natural Natural Void)
-  (lambda [/dev/stdout flag alias transform descriptions0 this-width max-width]
+(define cmdopt-display-flags : (-> Output-Port (Pairof (U Char Symbol) (Listof (U Char Symbol))) (Listof String) Natural Natural Void)
+  (lambda [/dev/stdout flags descriptions0 this-width max-width]
+    (define-values (flag alias) (values (car flags) (cdr flags)))
+    
     (fprintf /dev/stdout "    ~a~a" (if (char? flag) #\- '--) flag)
     (for ([alias (in-list alias)])
       (fprintf /dev/stdout ", ~a~a" (if (char? alias) #\- '--) alias))
