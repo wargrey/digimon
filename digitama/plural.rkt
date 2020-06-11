b203e1f9b7410cdf0ba78e18ab37a4df334db858
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2020-06-11 14:24:29 +0800

    add singular

--- digitama/plural.rkt
+++ digitama/plural.rkt
@@ -7,3 +7,15 @@
     (define dict : (HashTable String String) #hash(("story" . "stories") ("Story" . "Stories")))
     (cond [(= n 1) word]
           [else (hash-ref dict word (λ _ (string-append word "s")))])))
+
+(define singular : (-> String String)
+  (lambda [words]
+    (define size : Index (string-length words))
+    (or (case size
+          [(0 1) words]
+          [(3) (and (string-ci=? words "ies") (substring words 0 (sub1 size)))]
+          [else #false])
+        (let ([slast (string-ref words (sub1 size))])
+          (cond [(not (eq? slast #\s)) words]
+                [(regexp-match? #px"ies$" words) (string-append (substring words 0 (- size 3)) "y")]
+                [else (substring words 0 (sub1 size))])))))
