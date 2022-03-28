8b31f401c7fb2fa3af3febf9b7ebaeb0d34b30ad
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-03-28 13:17:02 +0800

    add foreign.rkt

--- foreign.rkt
+++ foreign.rkt
@@ -4,12 +4,12 @@
 
 (require/typed/provide
  ffi/unsafe
- [compiler-sizeof (-> Symbol Byte)])
+ [compiler-sizeof (-> (U Symbol (Listof Symbol)) Byte)])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define compiler-aligned-offset : (-> (U Symbol Index) Nonnegative-Fixnum Nonnegative-Fixnum)
+(define compiler-aligned-offset : (-> (U (U Symbol (Listof Symbol)) Index) Nonnegative-Fixnum Nonnegative-Fixnum)
   (lambda [type/size offset0]
-    (define type-size (if (symbol? type/size) (compiler-sizeof type/size) type/size))
+    (define type-size (if (index? type/size) type/size (compiler-sizeof type/size)))
     (define size-mod (remainder offset0 type-size))
     
     (cond [(= size-mod 0) offset0]
