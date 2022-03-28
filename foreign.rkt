75eff73782b806213bbbf4b7bc93facbe817c7aa
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-03-28 12:42:48 +0800

    add foreign.rkt

--- foreign.rkt
+++ foreign.rkt
@@ -7,12 +7,12 @@
  [compiler-sizeof (-> Symbol Byte)])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define compiler-aligned-offset : (-> (U Symbol Byte) Nonnegative-Fixnum Nonnegative-Fixnum)
-  (lambda [type offset0]
-    (define sizeof (if (symbol? type) (compiler-sizeof type) type))
-    (define sizemod (remainder offset0 sizeof))
+(define compiler-aligned-offset : (-> (U Symbol Index) Nonnegative-Fixnum Nonnegative-Fixnum)
+  (lambda [type/size offset0]
+    (define type-size (if (symbol? type/size) (compiler-sizeof type/size) type/size))
+    (define size-mod (remainder offset0 type-size))
     
-    (cond [(= sizemod 0) offset0]
-          [else (let ([padsize (- sizeof sizemod)])
+    (cond [(= size-mod 0) offset0]
+          [else (let ([padsize (- type-size size-mod)])
                   (cond [(> padsize 0) (assert (+ offset0 padsize) fixnum?)]
                         [else '#:deadcode offset0]))])))
