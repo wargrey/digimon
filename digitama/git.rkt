cccd8be27cb7a4d3143bad5f2badc7d935cf9f01
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-12-14 23:59:14 +0800

    finish dealing with renaming for `git-numstat`

--- digitama/git.rkt
+++ digitama/git.rkt
@@ -3,17 +3,22 @@
 (provide (all-defined-out) s/day)
 
 (require racket/string)
+(require racket/match)
 
 (require "../number.rkt")
 (require "../date.rkt")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define-type Git-Numstat-Line (List Natural Natural String))
+(define-type Git-Numstat-Line (List Natural Natural (U String (Pairof String String))))
 (define-type Git-Numstat (Pairof Natural (Listof Git-Numstat-Line)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define git:%at : String "--pretty=format:%at")
+(define px:rename:normal : Regexp #px"[{](.+) => (.+)[}]")
+(define px:rename:sub : Regexp #px"/[{] => (.+)[}]")
+(define px:rename:sup : Regexp #px"[{](.+) => [}]/")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define git-numstat-make-fold : (-> (Option Natural) (-> String (Listof Git-Numstat) (Listof Git-Numstat)))
   (lambda [group-span:s]
     (define make-empty-numstat : (-> Natural Git-Numstat)
@@ -23,35 +28,47 @@
               null)))
     
     (λ [line stats]
-      (define-values (#{self : Git-Numstat} rest)
+      (define-values (self rest)
         (cond [(pair? stats) (values (car stats) (cdr stats))]
               [else (values (make-empty-numstat (max (current-seconds) 0)) null)]))
       
-      (define tokens : (Listof String) (git-numstat-line-split line))
-      
-      (cond [(= (length tokens) 3)
-             (let ([insertion (string->natural (car tokens))]
-                   [deletion (string->natural (cadr tokens))])
-               (cond [(and insertion deletion)
-                      (let ([self++ ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (cons (list insertion deletion (caddr tokens)) (cdr self)))])
-                        (cons self++ rest))]
-                     [else (cons self rest)]))]
-            [(pair? tokens)
-             ;;; another timestamp follows closely, rarely happens. or,
-             ;;; `git log` always provides timestamps whereas `git diff` doesn't
-             (let ([timestamp (string->natural line)])
-               (cond [(not timestamp) '#:deadcode (cons self rest)]
-                     [(null? (cdr self)) (cons (make-empty-numstat timestamp) rest)]
-                     [else (cons (make-empty-numstat timestamp) (cons self rest))]))]
-            [else (cons ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (reverse (cdr self))) rest)]))))
+      (let ([tokens (git-numstat-line-split line)])
+        (cond [(list? tokens)
+               (let ([insertion (string->natural (car tokens))]
+                     [deletion (string->natural (cadr tokens))])
+                 (cond [(and insertion deletion)
+                        (let ([self++ ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (cons (list insertion deletion (caddr tokens)) (cdr self)))])
+                          (cons self++ rest))]
+                       [else (cons self rest)]))]
+              [(string? tokens)
+               ;;; another timestamp follows closely, rarely happens. or,
+               ;;; `git log` always provides timestamps whereas `git diff` doesn't
+               (let ([timestamp (string->natural tokens)])
+                 (cond [(not timestamp) '#:deadcode (cons self rest)]
+                       [(null? (cdr self)) (cons (make-empty-numstat timestamp) rest)]
+                       [else (cons (make-empty-numstat timestamp) (cons self rest))]))]
+              [(vector? tokens)
+               (let ([insertion (string->natural (vector-ref tokens 0))]
+                     [deletion (string->natural (vector-ref tokens 1))])
+                 (cond [(and insertion deletion)
+                        (let ([self++ ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (cons (list insertion deletion (vector-ref tokens 2)) (cdr self)))])
+                          (cons self++ rest))]
+                       [else (cons self rest)]))]
+              [else (cons ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (reverse (cdr self))) rest)])))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define git-numstat-line-split : (-> String (Listof String))
+(define git-numstat-line-split : (-> String (U False String (List String String String) (Vector String String (Pairof String String))))
   (lambda [line]
-    ; Note: lines like `59 53 {src.ext => renamed.ext}` are only produced by `git diff`
     ; TODO: seriously deal with spaces in filename
-    (define tokens : (Listof String) (string-split line))
-    (cond [(<= (length tokens) 3) tokens]
-          [(regexp-match? #px" => " line) (list (car tokens) (cadr tokens) (caddr tokens))] ; TODO: correct?
-          [else (list (car tokens) (cadr tokens) (string-join (cddr tokens) " "))])))
-
+    (match (string-split line)
+      [(list a d f) (list a d f)]
+      [(list ts) ts]
+      [(list a d f "=>" rest ...)
+       (let ([raw (string-join (cons f (cons "=>" rest)))])
+         (cond [(regexp-match? px:rename:normal raw) (vector a d (cons (regexp-replace px:rename:normal raw "\\1") (regexp-replace px:rename:normal raw "\\2")))]
+               [(regexp-match? px:rename:sub raw) (vector a d (cons (regexp-replace px:rename:sub raw "") (regexp-replace px:rename:sub raw "/\\1")))]
+               [(regexp-match? px:rename:sup raw) (vector a d (cons (regexp-replace px:rename:sup raw "\\1/") (regexp-replace px:rename:sup raw "")))]
+               [else #false]))]
+      [(list a d f ...) (list a d (string-join f " "))]
+      [_ #false])))
+  
--- git.rkt
+++ git.rkt
@@ -16,10 +16,11 @@
           [else (let-values ([(base name dir?) (split-path (simple-form-path dir))])
                   (and (path? base) (git-root base)))])))
 
-(define git-numstat : (-> [#:group-by-day? Boolean] (Listof Git-Numstat))
-  (lambda [#:group-by-day? [day? #true]]
+(define git-numstat : (-> [#:group-by-day? Boolean] [#:no-renames? Boolean] (Listof Git-Numstat))
+  (lambda [#:group-by-day? [day? #true] #:no-renames? [no-rename? #false]]
     (define git (find-executable-path "git"))
     (cond [(or (not (git-root)) (not git)) null]
-          [else (let ([numstat-fold (git-numstat-make-fold (and day? s/day))])
-                  (reverse (fg-recon-exec* 'git-numstat git (list (list "log") (list git:%at "--numstat" "--no-merges")) numstat-fold
-                                           (fg-recon-exec* 'git-numstat git (list (list "diff") (list "--numstat")) numstat-fold null))))])))
+          [else (let ([numstat-fold (git-numstat-make-fold (and day? s/day))]
+                      [no-renames (if no-rename? (list "--no-renames") null)])
+                  (reverse (fg-recon-exec* 'git-numstat git (list (list "log") (list git:%at "--numstat" "--no-merges") no-renames) numstat-fold
+                                           (fg-recon-exec* 'git-numstat git (list (list "diff") (list "--numstat") no-renames) numstat-fold null))))])))
