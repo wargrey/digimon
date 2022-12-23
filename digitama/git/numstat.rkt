f3fe4262e29f33a8b043e311281eea5a6c015ab9
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2022-12-23 23:42:49 +0800

    support submodules for `git-numstat` and `git-langstat`

--- digitama/git/numstat.rkt
+++ digitama/git/numstat.rkt
@@ -20,9 +20,11 @@
 (define git-silents : (Listof Exec-Silent) (list 'stdout 'stderr))
 
 (define git:%at : String "--pretty=format:%at")
-(define px:rename:normal : Regexp #px"[{](.+) => (.+)[}]")
+(define px:rename:normal : Regexp #px"[{]?(.+) => (.+)[}]?")
 (define px:rename:sub : Regexp #px"/[{] => (.+)[}]")
 (define px:rename:sup : Regexp #px"[{](.+) => [}]/")
+(define px:submodule:status : Regexp #px"^\\s?\\S+\\s([^(]+)\\s[(][^)]+[)]$")
+(define px:submodule:sync : Regexp #px"^[^']+'([^.]+)'$")
 
 (define current-git-procedure : (Parameterof Any) (make-parameter 'git-numstat))
 
@@ -33,45 +35,88 @@
      #:silent git-silents (current-git-procedure)
      git options numstat-fold initial)))
 
+(define git-submodule-list : (-> Path (Listof String))
+  (lambda [git]
+    (reverse
+     ((inst fg-recon-exec* (Listof String))
+      #:silent git-silents (current-git-procedure)
+      git (list (list "submodule" "status"))
+      git-submodule-name-fold null))))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define git-numstat-make-fold : (-> (Option Natural) (-> String (Listof Git-Numstat) (Listof Git-Numstat)))
-  (lambda [group-span:s]
+(define git-numstat-make-fold : (-> (Option Natural) (Option String) (-> String (Listof Git-Numstat) (Listof Git-Numstat)))
+  (lambda [group-span:s subpath/]
+    (define adjust-timestamp : (case-> [Natural -> Natural]
+                                       [(Option Natural) -> (Option Natural)])
+      (λ [timestamp]
+        (and timestamp
+             (cond [(not group-span:s) timestamp]
+                   [else (floor-seconds timestamp group-span:s)]))))
+
+    (define adjust-path : (-> (U String (Pairof String String)) (U String (Pairof String String)))
+      (cond [(not subpath/) values]
+            [else (λ [path] (cond [(string? path) (string-append subpath/ path)]
+                                  [else (cons (string-append subpath/ (car path))
+                                              (string-append subpath/ (cdr path)))]))]))
+
     (define make-empty-numstat : (-> Natural Git-Numstat)
-      (lambda [timestamp]
-        (cons (cond [(not group-span:s) timestamp]
-                    [else (floor-seconds timestamp group-span:s)])
-              null)))
-    
+      (λ [timestamp]
+        (cons timestamp null)))
+
     (λ [line stats]
       (define-values (self rest)
         (cond [(pair? stats) (values (car stats) (cdr stats))]
-              [else (values (make-empty-numstat (max (current-seconds) 0)) null)]))
+              [else (values (make-empty-numstat (adjust-timestamp (max (current-seconds) 0))) null)]))
       
       (let ([tokens (git-numstat-line-split line)])
-        (cond [(list? tokens)
+        (cond [(list? tokens) ; normal
                (let ([insertion (string->natural (car tokens))]
                      [deletion (string->natural (cadr tokens))])
                  (cond [(and insertion deletion)
-                        (let ([self++ ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (cons (vector-immutable insertion deletion (caddr tokens)) (cdr self)))])
-                          (cons self++ rest))]
+                        (let ([num-line (vector-immutable insertion deletion (adjust-path (caddr tokens)))])
+                          (cons ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (cons num-line (cdr self))) rest))]
                        [else (cons self rest)]))]
               [(string? tokens)
                ;;; another timestamp follows closely, rarely happens. or,
                ;;; `git log` always provides timestamps whereas `git diff` doesn't
-               (let ([timestamp (string->natural tokens)])
+               (let ([timestamp (adjust-timestamp (string->natural tokens))])
                  (cond [(not timestamp) '#:deadcode (cons self rest)]
+                       [(= timestamp (car self)) (cons self rest)]
                        [(null? (cdr self)) (cons (make-empty-numstat timestamp) rest)]
                        [else (cons (make-empty-numstat timestamp) (cons self rest))]))]
-              [(vector? tokens)
+              [(vector? tokens) ; rename
                (let ([insertion (string->natural (vector-ref tokens 0))]
                      [deletion (string->natural (vector-ref tokens 1))])
                  (cond [(and insertion deletion)
-                        (let ([self++ ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (cons (vector-immutable insertion deletion (vector-ref tokens 2)) (cdr self)))])
-                          (cons self++ rest))]
+                        (let ([rename-num-line (vector-immutable insertion deletion (adjust-path (vector-ref tokens 2)))])
+                          (cons ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (cons rename-num-line (cdr self))) rest))]
                        [else (cons self rest)]))]
               [else (cons ((inst cons Natural (Listof Git-Numstat-Line)) (car self) (reverse (cdr self))) rest)])))))
 
+(define git-submodule-name-fold : (-> String (Listof String) (Listof String))
+  (lambda [line names]
+    (define maybe-name (regexp-match px:submodule:status line))
+    
+    (cond [(not maybe-name) names]
+          [else (cons (assert (cadr maybe-name)) names)])))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define git-numatat-merge : (-> (Listof Git-Numstat) (Listof Git-Numstat) Boolean (Listof Git-Numstat))
+  (lambda [lstats rstats reverse?]
+    (define lt? (if reverse? > <))
+    (let merge ([lstats : (Listof Git-Numstat) lstats]
+                [rstats : (Listof Git-Numstat) rstats]
+                [stats : (Listof Git-Numstat) null])
+      (cond [(null? lstats) (append (reverse stats) rstats)]
+            [(null? rstats) (append (reverse stats) lstats)]
+            [else (let*-values ([(lstat lrest) (values (car lstats) (cdr lstats))]
+                                [(rstat rrest) (values (car rstats) (cdr rstats))]
+                                [(lself rself) (values (car lstat) (car rstat))])
+                    (cond [(lt? lself rself) (merge lstats rrest (cons rstat stats))]
+                          [(not (= lself rself)) (merge lrest rstats (cons lstat stats))]
+                          [else (let ([self ((inst cons Natural (Listof Git-Numstat-Line)) lself (append (cdr lstat) (cdr rstat)))])
+                                  (merge lrest rrest (cons self stats)))]))]))))
+
 (define git-numstat-line-split : (-> String (U False String (List String String String) (Vector String String (Pairof String String))))
   (lambda [line]
     ; TODO: seriously deal with spaces in filename
--- git.rkt
+++ git.rkt
@@ -7,28 +7,42 @@
 (require racket/path)
 (require racket/port)
 
+(require "dtrace.rkt")
+(require "filesystem.rkt")
+
 (require "digitama/git/numstat.rkt")
 (require "digitama/git/langstat.rkt")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define git-root : (->* () (Path-String) (Option Path-String))
+(define git-root : (->* () (Path-String) (Option Path))
   (lambda [[dir (current-directory)]]
     (define .git (build-path dir ".git"))
-    (cond [(directory-exists? .git) .git]
+    (cond [(directory-exists? .git) (if (path? dir) dir (string->path dir))]
+          [(file-exists? .git) (if (path? dir) dir (string->path dir))]
           [else (let-values ([(base name dir?) (split-path (simple-form-path dir))])
                   (and (path? base) (git-root base)))])))
 
-(define git-numstat : (-> [#:group-by-day? Boolean] [#:no-renames? Boolean] [#:since (Option Git-Date-Datum)] [#:until (Option Git-Date-Datum)] [#:localtime? Boolean] 
-                          [#:n (Option Natural)] [#:authors (U String (Listof String))] [#:committers (U String (Listof String))] [#:with-diff? Boolean]
-                          [#:git-reverse? Boolean] [#:rkt-reverse? Boolean]
-                          (Listof Git-Numstat))
+(define git-submodule : (->* () (Path-String) (Listof String))
+  (lambda [[dir (current-directory)]]
+    (define git (find-executable-path "git"))
+    (define rootdir : (Option Path) (git-root dir))
+    (cond [(or (not rootdir) (not git)) null]
+          [else (parameterize ([current-directory rootdir])
+                  (git-submodule-list git))])))
+
+(define git-numstat : (->* ()
+                           (Path-String #:group-by-day? Boolean #:no-renames? Boolean #:since (Option Git-Date-Datum) #:until (Option Git-Date-Datum) #:localtime? Boolean
+                                        #:n (Option Natural) #:authors (U String (Listof String)) #:committers (U String (Listof String)) #:with-diff? Boolean
+                                        #:git-reverse? Boolean #:rkt-reverse? Boolean #:recursive? Boolean) 
+                           (Listof Git-Numstat))
   (lambda [#:no-renames? [no-renames? #false] #:group-by-day? [day? #true] #:since [since #false] #:until [until #false] #:localtime? [localtime? #false]
            #:n [n #false] #:authors [authors null] #:committers [committers null] #:with-diff? [diff? (and (null? authors) (null? committers) (not until))]
-           #:git-reverse? [git-reverse? #false] #:rkt-reverse? [rkt-reverse? #true]]
+           #:git-reverse? [git-reverse? #false] #:rkt-reverse? [rkt-reverse? #true] #:recursive? [recursive? #true]
+           [dir (current-directory)]]
     (define git (find-executable-path "git"))
-    (cond [(or (not (git-root)) (not git)) null]
-          [else (let ([numstat-fold (git-numstat-make-fold (and day? s/day))]
-                      [opt:log (list "log" git:%at "--numstat" "--no-merges" "--extended-regexp")]
+    (define rootdir : (Option Path) (git-root dir))
+    (cond [(or (not rootdir) (not git)) null]
+          [else (let ([opt:log (list "log" git:%at "--numstat" "--no-merges" "--extended-regexp")]
                       [opt:n (if n (list (string-append "--max-count=" (number->string n))) null)]
                       [opt:since (if since (git-numstat-date-option "since" since localtime?) null)]
                       [opt:until (if until (git-numstat-date-option "until" until localtime?) null)]
@@ -36,22 +50,45 @@
                       [opt:rename (if no-renames? (list "--no-renames") null)]
                       [opt:author (for/list : (Listof String) ([a (if (list? authors) (in-list authors) (in-value authors))]) (string-append "--author=" a))]
                       [opt:committer (for/list : (Listof String) ([a (if (list? committers) (in-list committers) (in-value committers))]) (string-append "--committer=" a))])
-                  ((if rkt-reverse? reverse values)
-                   (git-numstat-exec git (list opt:log opt:n opt:since opt:until opt:reverse opt:rename opt:author opt:committer) numstat-fold
-                                     (cond [(and diff?) (git-numstat-exec git (list (list "diff" "--numstat") opt:rename) numstat-fold null)]
-                                           [else null]))))])))
 
-(define git-langstat : (-> [#:group-by-day? Boolean] [#:no-renames? Boolean] [#:since (Option Git-Date-Datum)] [#:until (Option Git-Date-Datum)] [#:localtime? Boolean] 
-                           [#:n (Option Natural)] [#:authors (U String (Listof String))] [#:committers (U String (Listof String))] [#:with-diff? Boolean]
-                           [#:grouping-option Git-Langstat-Grouping-Option] [#:types (Listof Symbol)] [#:reverse? Boolean]
-                           (Immutable-HashTable Natural Git-Language))
+                  (define (git-numstat [rootdir : Path] [subpath/ : (Option String)]) : (Listof Git-Numstat)
+                    (parameterize ([current-directory rootdir])
+                      (define numstat-fold (git-numstat-make-fold (and day? s/day) subpath/))
+                      
+                      (git-numstat-exec git (list opt:log opt:n opt:since opt:until opt:reverse opt:rename opt:author opt:committer) numstat-fold
+                                        (cond [(and diff?) (git-numstat-exec git (list (list "diff" "--numstat") opt:rename) numstat-fold null)]
+                                              [else null]))))
+
+                  (define (git-numstat-recursive [rootdir : Path] [subpath/ : (Option String)]) : (Listof Git-Numstat)
+                    (parameterize ([current-directory rootdir])
+                      (dtrace-note #:topic (current-git-procedure) "cd ~a" rootdir)
+                      (for/fold ([stats : (Listof Git-Numstat) (git-numstat rootdir subpath/)])
+                                ([subname (in-list (git-submodule-list git))])
+                        (define subrootdir : Path (build-path rootdir (path-normalize/system subname)))
+                        (define submodpath : String (if (not subpath/) (string-append subname "/") (string-append subpath/ subname "/")))
+
+                        (git-numatat-merge stats (git-numstat-recursive subrootdir submodpath) git-reverse?))))
+
+                  ((if rkt-reverse? reverse values)
+                   (cond [(not recursive?) (git-numstat rootdir #false)]
+                         [else (git-numstat-recursive rootdir #false)])))])))
+
+(define git-langstat : (->* ()
+                            (Path-String #:group-by-day? Boolean #:no-renames? Boolean #:since (Option Git-Date-Datum) #:until (Option Git-Date-Datum) #:localtime? Boolean 
+                                         #:n (Option Natural) #:authors (U String (Listof String)) #:committers (U String (Listof String)) #:with-diff? Boolean
+                                         #:grouping-option Git-Langstat-Grouping-Option #:types (Listof Symbol) #:reverse? Boolean #:recursive? Boolean)
+                            (Immutable-HashTable Natural Git-Language))
   (lambda [#:no-renames? [no-renames? #false] #:group-by-day? [day? #true] #:since [since #false] #:until [until #false] #:localtime? [localtime? #false]
            #:n [n #false] #:authors [authors null] #:committers [committers null] #:with-diff? [diff? (and (null? authors) (null? committers) (not until))]
-           #:grouping-option [grouping-opt '(["Scrbble" . #".scrbl"])] #:types [types null] #:reverse? [reverse? #false]]
+           #:grouping-option [grouping-opt '(["Scribble" . #".scrbl"])] #:types [types null] #:reverse? [reverse? #false] #:recursive? [recursive? #true]
+           [dir (current-directory)]]
     (parameterize ([current-git-procedure git-langstat])
       (define numstats : (Listof Git-Numstat)
         (git-numstat #:no-renames? no-renames? #:with-diff? diff? #:authors authors #:committers committers
                      #:group-by-day? day? #:since since #:until until #:localtime? localtime? #:n n
-                     #:git-reverse? reverse? #:rkt-reverse? #false))
+                     #:git-reverse? reverse? #:rkt-reverse? #false #:recursive? recursive?
+                     dir))
 
       (git-numstats->langstat numstats types grouping-opt))))
+
+(git-langstat #:since -30 "/Users/wargrey/Laboratory/YouthLanguage")
