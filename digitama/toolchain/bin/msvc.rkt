4a0b88113ae51b2aeb62517b15af0a91446b136d
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-02-11 11:39:24 +0800

    fix msvc flags

--- digitama/toolchain/bin/msvc.rkt
+++ digitama/toolchain/bin/msvc.rkt
@@ -26,7 +26,7 @@
     (append (list "/nologo" "/FC" "/c" ; compiling only, no link
                   #;"/constexpr"
                   "/EHsc" "/W3" "/sdl" #;'| security features and warnings |)
-            (if (not debug?) (list "/O2 /GL /Zi") (list "/Od" "/ZI" "/JMC"))
+            (if (not debug?) (list "/O2") (list "/Od" "/ZI" "/JMC"))
             (if (not cpp?) (list "/TC" "/std:c17") (list "/TP" "/std:c++17"))
             (if (not verbose?) null (list "/showIncludes")))))
 
