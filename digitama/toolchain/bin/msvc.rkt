8092b8e76a6cf670a7bd4c9f634e3f6ffcb94abe
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2023-07-08 20:27:38 +0800

    do not add space between `-D` and macro for MSVC

--- digitama/toolchain/bin/msvc.rkt
+++ digitama/toolchain/bin/msvc.rkt
@@ -84,7 +84,7 @@
     (define-values (name body) (values (car macro) (cdr macro)))
 
     (cond [(string-contains? name "(") (raise-user-error 'cc "function-like macro is not allowed: ~a." name)]
-          [(not body) (string-append "/D " name)]
+          [(not body) (string-append "/D" name)]
           [(string-contains? body " ") (string-append "/D\"" name "=" body "\"")]
           [else (string-append "/D" name "=" body)])))
 
