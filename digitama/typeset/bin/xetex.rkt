b2a4cae2e8249a3c6a35f0d0a9f97bf0280af4ad
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2024-03-31 22:47:50 +0800

    exCJK does not need fontenc

--- digitama/typeset/bin/xetex.rkt
+++ digitama/typeset/bin/xetex.rkt
@@ -6,6 +6,7 @@
 (define xetex-preamble-filter : Tex-Preamble-Filter
   (lambda [line status]
     (cond [(regexp-match? #px"\\\\usepackage\\[utf8\\][{]inputenc[}]" line) (values "\\usepackage{xeCJK}" 'xeCJKed)]
+          [(regexp-match? #px"\\\\usepackage\\[T1\\][{]fontenc[}]" line) (values #false 'commandset)]
           [(regexp-match? #px"\\\\newcommand[{]\\\\packageCJK[}]" line) (values #false 'commandset)]
           [(regexp-match? #px"\\\\packageCJK" line) (values #false 'EOF)]
           [else (values line status)])))
