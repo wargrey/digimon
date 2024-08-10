6903bcb1343fb33c258795c787cf4915008d3bc8
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2024-08-10 10:53:21 +0800

    add `metrics.rkt`

---
+++ metrics.rkt
@@ -0,0 +1,27 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require racket/math)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define ~radian : (case-> [Real -> Flonum]
+                          [Real Boolean -> Flonum])
+  (case-lambda
+    [(degree) (* (real->double-flonum degree) (/ pi 180.0))]
+    [(angle radian?) (if (and radian?) (real->double-flonum angle) (~radian angle))]))
+
+(define ~length : (case-> [Real -> Nonnegative-Flonum]
+                          [Real Nonnegative-Flonum -> Nonnegative-Flonum])
+  (case-lambda
+    [(fl) (if (> fl 0.0) (real->double-flonum fl) 0.0)]
+    [(fl% 100%)
+     (cond [(< fl% 0.0) (* (abs (real->double-flonum fl%)) 100%)]
+           [(>= fl% 0.0) (real->double-flonum fl%)]
+           [else #| nan |# 0.0])]))
+
+(define ~size : (case-> [Real -> Nonnegative-Flonum]
+                        [Real Real -> (Values Nonnegative-Flonum Nonnegative-Flonum)])
+  (case-lambda
+    [(w) (~length w)]
+    [(w h) (let ([width (~length w)]) (values width (~length h width)))]))
