56e8efd6ca7f2b252145872b49d7673ee9c34cf6
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2024-08-14 23:10:14 +0800

    add `sequence.rkt`

---
+++ sequence.rkt
@@ -0,0 +1,34 @@
+#lang typed/racket/base
+
+(provide (all-defined-out))
+
+(require racket/list)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define #:forall (N) list->4:values : (-> (Listof N) N (Values N N N N))
+  (lambda [ls defval]
+    (cond [(null? ls) (values defval defval defval defval)]
+          [(null? (cdr ls)) (let ([top (car ls)]) (values top top top top))]
+          [(null? (cddr ls)) (let ([top (car ls)] [right (cadr ls)]) (values top right top right))]
+          [(null? (cdddr ls)) (let ([top (car ls)] [right (cadr ls)] [bottom (caddr ls)]) (values top right bottom right))]
+          [else (values (car ls) (cadr ls) (caddr ls) (cadddr ls))])))
+
+(define #:forall (T) list->n:vector : (case-> [(Pairof T (Listof T)) Index -> (Vectorof T)]
+                                              [(Listof T) Index T -> (Vectorof T)])
+  (case-lambda
+    [(src total)
+     (for/vector : (Vectorof T) #:length total #:fill (last src)
+       ([datum (in-list src)]) datum)]
+    [(src total supplement)
+     (for/vector : (Vectorof T) #:length total #:fill supplement
+       ([datum (in-list src)]) datum)]))
+
+(define #:forall (T D) list->n:vector* : (case-> [(Pairof T (Listof T)) Index (-> T D) -> (Vectorof D)]
+                                                 [(Listof T) Index D (-> T D) -> (Vectorof D)])
+  (case-lambda
+    [(src total ->v)
+     (for/vector : (Vectorof D) #:length total #:fill (->v (last src))
+       ([datum (in-list src)]) (->v datum))]
+    [(src total supplement ->v)
+     (for/vector : (Vectorof D) #:length total #:fill supplement
+       ([datum (in-list src)]) (->v datum))]))
