fb2e40848ad6f9fb928b765d628a037af05cff33
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2025-03-31 20:38:43 +0800

    `subfigure` recognizes `compound-paragraph`

--- digitama/tamer/subfigure.rkt
+++ digitama/tamer/subfigure.rkt
@@ -16,7 +16,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define subfigure-flows
   (lambda [pre-flows substyle [fmt (tamer-subfigure-index-format)] [sep #false] [order values]]
-    (define subfigures (decode-flow pre-flows))
+    (define subfigures (apply append (map subfigure-flatten (decode-flow pre-flows))))
     (define n (length subfigures))
 
     (if (> n 1)
@@ -27,21 +27,35 @@
                                              [serugif null]
                                              [slebal null])
                          (cond [(pair? subs)
-                                (let* ([self (paragraph-content (car subs))]
-                                       [subcap (cdr self)]
-                                       [subidx (string (integer->char idx))]
-                                       [subtag (tamer-subfigure-tag (current-block-id) subidx)])
-                                  (make-subfigures (cdr subs) (add1 idx)
-                                                   (cons (car self) serugif)
-                                                   (cons (if (pair? subcap)
-                                                             (elem #:style substyle
-                                                                   (tamer-elemtag subtag (format fmt subidx))
-                                                                   ~ subcap)
-                                                             null)
-                                                         slebal)))]
+                                (let-values ([(self rest) (values (car subs) (cdr subs))])
+                                  (if (paragraph? self)
+                                      (let-values ([(sub caption) (subfigure-extract self idx substyle fmt)])
+                                        (make-subfigures rest (add1 idx) (cons sub serugif) (cons caption slebal)))
+                                      (make-subfigures rest (add1 idx) (cons self serugif) (cons null slebal))))]
                                [(andmap null? slebal) (list (reverse serugif))]
                                [else (order (list (reverse serugif) (reverse slebal)))]))))
         subfigures)))
+
+(define subfigure-extract
+  (lambda [p idx substyle fmt]
+    (define self (paragraph-content p))
+    (define subcap (cdr self))
+    (define subidx (string (integer->char idx)))
+    (define subtag (tamer-subfigure-tag (current-block-id) subidx))
+    
+    (values (car self)
+            (if (pair? subcap)
+                (centered (para #:style substyle
+                                (tamer-elemtag subtag (format fmt subidx))
+                                ~ subcap))
+                null))))
+
+; Yes, only flatten the top-level block
+(define subfigure-flatten
+  (lambda [self]
+    (if (compound-paragraph? self)
+        (compound-paragraph-blocks self)
+        (list self))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define tamer-subfigure-tag
