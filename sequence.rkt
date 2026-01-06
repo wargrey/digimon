664310b4c5116caafa27a895a1f13ee266177f34
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2026-01-06 14:10:28 +0800

    improved `list->4:values` and `vector->4:values`

--- sequence.rkt
+++ sequence.rkt
@@ -19,13 +19,30 @@
     [(v ls ?) (if (and v (? v)) (cons v ls) ls)]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define #:forall (N) list->4:values : (-> (Listof N) N (Values N N N N))
-  (lambda [ls defval]
-    (cond [(null? ls) (values defval defval defval defval)]
-          [(null? (cdr ls)) (let ([top (car ls)]) (values top top top top))]
-          [(null? (cddr ls)) (let ([top (car ls)] [right (cadr ls)]) (values top right top right))]
-          [(null? (cdddr ls)) (let ([top (car ls)] [right (cadr ls)] [bottom (caddr ls)]) (values top right bottom right))]
-          [else (values (car ls) (cadr ls) (caddr ls) (cadddr ls))])))
+(define #:forall (T V) list->4:values : (case-> [(Listof V) V -> (Values V V V V)]
+                                                [(Listof T) V (-> T V) -> (Values V V V V)])
+  (case-lambda
+    [(ls defval)
+     (cond [(null? ls) (values defval defval defval defval)]
+           [(null? (cdr ls)) (let ([top (car ls)]) (values top top top top))]
+           [(null? (cddr ls)) (let ([top (car ls)] [right (cadr ls)]) (values top right top right))]
+           [(null? (cdddr ls)) (let ([top (car ls)] [right (cadr ls)] [bottom (caddr ls)]) (values top right bottom right))]
+           [else (values (car ls) (cadr ls) (caddr ls) (cadddr ls))])]
+    [(ls defval ->value)
+     (cond [(null? ls) (values defval defval defval defval)]
+           [(null? (cdr ls))
+            (let ([top (->value (car ls))])
+              (values top top top top))]
+           [(null? (cddr ls))
+            (let ([top (->value (car ls))]
+                  [right (->value (cadr ls))])
+              (values top right top right))]
+           [(null? (cdddr ls))
+            (let ([top (->value (car ls))]
+                  [right (->value (cadr ls))]
+                  [bottom (->value (caddr ls))])
+              (values top right bottom right))]
+           [else (values (->value (car ls)) (->value (cadr ls)) (->value (caddr ls)) (->value (cadddr ls)))])]))
 
 (define #:forall (T) list->n:vector : (case-> [(Pairof T (Listof T)) Natural -> (Vectorof T)]
                                               [(Listof T) Natural T -> (Vectorof T)])
@@ -48,15 +65,32 @@
        ([datum (in-list src)]) (->v datum))]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define #:forall (N) vector->4:values : (-> (Vectorof N) N (Values N N N N))
-  (lambda [vs defval]
-    (define size (vector-length vs))
-    
-    (cond [(zero? size) (values defval defval defval defval)]
-          [(= size 1) (let ([top (vector-ref vs 0)]) (values top top top top))]
-          [(= size 2) (let ([top (vector-ref vs 0)] [right (vector-ref vs 1)]) (values top right top right))]
-          [(= size 3) (let ([top (vector-ref vs 0)] [right (vector-ref vs 1)] [bottom (vector-ref vs 2)]) (values top right bottom right))]
-          [else (values (vector-ref vs 0) (vector-ref vs 1) (vector-ref vs 2) (vector-ref vs 3))])))
+(define #:forall (T V) vector->4:values : (case-> [(Vectorof V) V -> (Values V V V V)]
+                                                  [(Vectorof T) V (-> T V) -> (Values V V V V)])
+  (case-lambda
+    [(vs defval)
+     (let ([size (vector-length vs)])
+       (cond [(zero? size) (values defval defval defval defval)]
+             [(= size 1) (let ([top (vector-ref vs 0)]) (values top top top top))]
+             [(= size 2) (let ([top (vector-ref vs 0)] [right (vector-ref vs 1)]) (values top right top right))]
+             [(= size 3) (let ([top (vector-ref vs 0)] [right (vector-ref vs 1)] [bottom (vector-ref vs 2)]) (values top right bottom right))]
+             [else (values (vector-ref vs 0) (vector-ref vs 1) (vector-ref vs 2) (vector-ref vs 3))]))]
+    [(vs defval ->value)
+     (let ([size (vector-length vs)])
+       (cond [(zero? size) (values defval defval defval defval)]
+             [(= size 1)
+              (let ([top (->value (vector-ref vs 0))])
+                (values top top top top))]
+             [(= size 2)
+              (let ([top (->value (vector-ref vs 0))]
+                    [right (->value (vector-ref vs 1))])
+                (values top right top right))]
+             [(= size 3)
+              (let ([top (->value (vector-ref vs 0))]
+                    [right (->value (vector-ref vs 1))]
+                    [bottom (->value (vector-ref vs 2))])
+                (values top right bottom right))]
+             [else (values (->value (vector-ref vs 0)) (->value (vector-ref vs 1)) (->value (vector-ref vs 2)) (->value (vector-ref vs 3)))]))]))
 
 (define #:forall (T) vector->n:vector : (-> (Vectorof T) Natural T (Vectorof T))
   (lambda [src total supplement]
