a9f87a25221e52aef27915a6c94f39da67943fe2
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2026-01-13 01:00:37 +0800

    merge `constant.rkt` into `measure.rkt`

--- measure.rkt
+++ measure.rkt
@@ -17,6 +17,18 @@
 (define-type ~Length+% (U Real ~% ~L))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define-values (-pi/2 pi/2 3pi/2 2pi -2pi pi/4 3pi/4 2pi/5 4pi/5)
+  (values (* pi -0.5) (* pi +0.5)
+          (* pi +1.5) (* pi +2.0) (* pi -2.0)
+          (* pi 0.25) (* pi 0.75)
+          (* pi 0.40) (* pi 0.80)))
+
+(define phi : Nonnegative-Flonum (* (+ 1.0 (sqrt 5.0)) 0.5))
+(define 1/phi : Nonnegative-Flonum (/ 2.0 (+ 1.0 (sqrt 5.0))))
+(define -phi : Flonum (- phi))
+(define -1/phi : Flonum (- 1/phi))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define ~px : (case-> [~L -> Nonnegative-Flonum]
                       [Real Length-Unit -> Nonnegative-Flonum]
                       [~Real Length-Unit Nonnegative-Flonum -> Nonnegative-Flonum])
